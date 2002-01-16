266dbf0d4b0d111d8566765b0325f320ecec8d8c
Cornelius Schumacher <schumacher@kde.org>
2002-01-16 17:01:09 +0000

    Small modification to KOrganizer licence, which allows to link against non-GPL Qt versions. All copyright holders have agreed.
    svn path=/trunk/kdepim/; revision=131985

--- calprinter.h
+++ calprinter.h
@@ -1,20 +1,24 @@
 /*
-  This file is part of KOrganizer.
-  Copyright (c) 1998 Preston Brown
+    This file is part of KOrganizer.
+    Copyright (c) 1998 Preston Brown
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
-  GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+    As a special exception, permission is given to link this program
+    with any edition of Qt, and distribute the resulting executable,
+    without including the source code for Qt in the source distribution.
 */
 #ifndef _CALPRINTER_H
 #define _CALPRINTER_H
--- exportwebdialog.h
+++ exportwebdialog.h
@@ -15,6 +15,10 @@
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+    As a special exception, permission is given to link this program
+    with any edition of Qt, and distribute the resulting executable,
+    without including the source code for Qt in the source distribution.
 */
 #ifndef _EXPORTWEBDIALOG_H
 #define _EXPORTWEBDIALOG_H
