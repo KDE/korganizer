8f0760fb91bb0b91b012af5ffe69842f7be0ec05
Cornelius Schumacher <schumacher@kde.org>
2002-02-23 20:25:11 +0000

    Fixed ambigous text. Fixed location of plugin menu actions.
    svn path=/trunk/kdepim/; revision=138277

--- koeventpopupmenu.cpp
+++ koeventpopupmenu.cpp
@@ -40,7 +40,7 @@ KOEventPopupMenu::KOEventPopupMenu()
   mHasAdditionalItems = false;
 
   insertItem (i18n("&Show"),this,SLOT(popupShow()));
-  mEditOnlyItems.append(insertItem (i18n("&Edit"),this,SLOT(popupEdit())));
+  mEditOnlyItems.append(insertItem (i18n("edit event","&Edit"),this,SLOT(popupEdit())));
   mEditOnlyItems.append(insertItem (SmallIcon("editdelete"),i18n("&Delete"),
                                    this,SLOT(popupDelete())));
 }
