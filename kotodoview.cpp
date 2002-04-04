983351f403d4434c2b253ea789a54792f701e4de
Cornelius Schumacher <schumacher@kde.org>
2002-04-04 12:03:05 +0000

    Add info to ambigous string.
    svn path=/trunk/kdepim/; revision=146997

--- kotodoview.cpp
+++ kotodoview.cpp
@@ -284,9 +284,9 @@ KOTodoView::KOTodoView(Calendar *calendar,QWidget* parent,const char* name) :
   mPopupMenu = new QPopupMenu;
   mPopupMenu->insertItem(SmallIconSet("todo"), i18n("New To-Do"), this,
                          SLOT (newTodo()));
-  mPopupMenu->insertItem(i18n("Purge Completed"), this,
-                         SLOT(purgeCompleted()));
-
+  mPopupMenu->insertItem(i18n("delete completed To-Dos","Purge Completed"),
+                         this, SLOT(purgeCompleted()));
+  
   mDocPrefs = new DocPrefs( name );
 
   // Double clicking conflicts with opening/closing the subtree
