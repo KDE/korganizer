400444d072d7432f6c11eee555e1a93684981581
Cornelius Schumacher <schumacher@kde.org>
2002-07-03 21:08:22 +0000

    Print time zone information.
    svn path=/trunk/kdepim/; revision=164629

--- timezone.cpp
+++ timezone.cpp
@@ -1,8 +1,13 @@
+#include <time.h>
+
+#include <qdatetime.h>
+
 #include <kaboutdata.h>
 #include <kapplication.h>
 #include <kdebug.h>
 #include <klocale.h>
 #include <kcmdlineargs.h>
+#include <kglobal.h>
 
 #include "koprefs.h"
 
@@ -13,5 +18,23 @@ int main(int argc,char **argv)
 
   KApplication app;
 
-  kdDebug() << "TimezoneId: " << KOPrefs::instance()->mTimeZoneId << endl;
+  kdDebug() << "KOrganizer TimezoneId: " << KOPrefs::instance()->mTimeZoneId
+            << endl;
+  
+  time_t ltime;
+  ::time( &ltime );
+  tm *t = localtime( &ltime );
+
+  kdDebug() << "localtime: " << t->tm_hour << ":" << t->tm_min << endl;
+
+  kdDebug() << "tzname: " << tzname[0] << " " << tzname[1] << endl;
+  kdDebug() << "timezone: " << timezone/3600 << endl;
+  
+  QTime qtime = QTime::currentTime();
+  
+  kdDebug() << "QDateTime::currentTime(): "
+            << qtime.toString( Qt::ISODate ) << endl;
+
+  kdDebug() << "KLocale::formatTime(): "
+            << KGlobal::locale()->formatTime( qtime ) << endl;
 }
