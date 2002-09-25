47acc7d622bb68af91d46c454a07dcba9bb426bd
Stephan Kulow <coolo@kde.org>
2002-09-25 20:25:48 +0000

    -  mText += "<font color=\"white\"> What's next?</h1></font>"; +  mText += i18n("<font color=\"white\"> What's next?</h1></font>");
    svn path=/trunk/kdepim/; revision=180428

--- kowhatsnextview.cpp
+++ kowhatsnextview.cpp
@@ -117,7 +117,7 @@ void KOWhatsNextView::updateView()
   mText += "<img src=\"";
   mText += *ipath;
   mText += "\">";
-  mText += "<font color=\"white\"> What's next?</h1></font>";
+  mText += i18n("<font color=\"white\"> What's next?</h1></font>");
   mText += "</td></tr>\n<tr><td>";
   
   QPtrList<Event> events = calendar()->events( QDate::currentDate(), true );
