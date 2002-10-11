e2e3268e726e540913ad50fd8329604c98eb86c4
Cornelius Schumacher <schumacher@kde.org>
2002-10-11 22:31:13 +0000

    Document versioning scheme.
    svn path=/trunk/kdepim/; revision=183388

--- version.h
+++ version.h
@@ -1,6 +1,29 @@
 #ifndef KORG_VERSION_H
 #define KORG_VERSION_H
 
-static const char *korgVersion = "pre3.1rc1";
+/*
+  Version scheme: "x.y.z build".
+
+  "x.y.z" follow the kdelibs version KOrganizer is released with.
+  
+  If "z" is 0, it the version is "x.y"
+  
+  "build" is empty for final versions. For developer version "build" is
+  something like "alpha1", "alpha2", "beta1", "beta2", "rc1", "rc2".
+
+  Examples in chronological order:
+  
+    3.0
+    3.0.1
+    3.1 alpha1
+    3.1 beta1
+    3.1 beta2
+    3.1 rc1
+    3.1
+    3.1.1
+    3.2 alpha1
+*/
+
+static const char *korgVersion = "3.1 rc1";
 
 #endif
