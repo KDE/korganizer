70c5a15393939d0d97ff8e8d7ba6f5a84aa3e7d5
Dirk Mueller <mueller@kde.org>
2002-11-20 23:55:06 +0000

    fix compilation, don't require yacc
    svn path=/trunk/kdepim/; revision=189931

---
+++ plugins/holidays/parseholiday.h
@@ -0,0 +1,26 @@
+typedef union { int ival; char *sval; } YYSTYPE;
+#define	NUMBER	257
+#define	MONTH	258
+#define	WDAY	259
+#define	COLOR	260
+#define	STRING	261
+#define	IN	262
+#define	PLUS	263
+#define	MINUS	264
+#define	SMALL	265
+#define	CYEAR	266
+#define	LEAPYEAR	267
+#define	LENGTH	268
+#define	EASTER	269
+#define	EQ	270
+#define	NE	271
+#define	LE	272
+#define	GE	273
+#define	LT	274
+#define	GT	275
+#define	OR	276
+#define	AND	277
+#define	UMINUS	278
+
+
+extern YYSTYPE kcallval;
