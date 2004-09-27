118f10fcef5756886200e9a817bac7c663caf765
Reinhold Kainhofer <reinhold@kainhofer.com>
2004-09-27 11:41:56 +0000

    CVS_SILENT: Update copyright information.
    svn path=/trunk/kdepim/; revision=349656

--- koprefsdialog.cpp
+++ koprefsdialog.cpp
@@ -2,6 +2,7 @@
     This file is part of KOrganizer.
 
     Copyright (c) 2000-2003 Cornelius Schumacher <schumacher@kde.org>
+    Copyright (c) 2003-2004 Reinhold Kainhofer <reinhold@kainhofer.com>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
