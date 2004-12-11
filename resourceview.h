36b8b10c7965733bf88473ba66439fa4185ea86e
Till Adam <adam@kde.org>
2004-12-11 12:56:21 +0000

    re-remove the now really unused signal
    svn path=/trunk/kdepim/; revision=369847

--- resourceview.h
+++ resourceview.h
@@ -131,9 +131,6 @@ class ResourceView : public CalendarViewExtension
     void currentChanged( QListViewItem* );
     void slotSubresourceAdded( ResourceCalendar *, const QString &,
                                const QString &resource,const QString& label );
-    void slotSubresourceAdded( ResourceCalendar *, const QString &,
-                               const QString &resource);
-
     void slotSubresourceRemoved( ResourceCalendar *, const QString &,
                                  const QString & );
     void closeResource( ResourceCalendar * );
