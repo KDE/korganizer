2790f328a98f7a179a5b08b18921d99a1e700af6
George Staikos <staikos@kde.org>
2004-12-11 05:35:23 +0000

    compile
    svn path=/trunk/kdepim/; revision=369784

--- resourceview.h
+++ resourceview.h
@@ -131,6 +131,8 @@ class ResourceView : public CalendarViewExtension
     void currentChanged( QListViewItem* );
     void slotSubresourceAdded( ResourceCalendar *, const QString &,
                                const QString &resource,const QString& label );
+    void slotSubresourceAdded( ResourceCalendar *, const QString &,
+                               const QString &resource);
 
     void slotSubresourceRemoved( ResourceCalendar *, const QString &,
                                  const QString & );
