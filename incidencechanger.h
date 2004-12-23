07ddfc2df349c434aa7874a0d69d001369e0e4f0
Mario Teijeiro Otero <emeteo@escomposlinux.org>
2004-12-23 00:13:35 +0000

    * fix order params
    svn path=/trunk/kdepim/; revision=372897

--- incidencechanger.h
+++ incidencechanger.h
@@ -38,7 +38,7 @@ public:
   bool endChange( Incidence *incidence );
 
   bool addIncidence( Incidence *incidence );
-  bool changeIncidence( Incidence *newinc, Incidence *oldinc, int action = -1 );
+  bool changeIncidence( Incidence *oldinc, Incidence *newinc, int action = -1 );
   bool deleteIncidence( Incidence *incidence );
   
   bool cutIncidence( Incidence *incidence );
