6ad0f56b520fbbd2000c93d21143fc5c6cf5f88e
Allen Winter <winter@kde.org>
2006-04-01 19:25:45 +0000

    PROJECTVIEW -> TIMESPANVIEW
    svn path=/trunk/KDE/kdepim/; revision=525359

--- plugins/timespanview/timespanview.h
+++ plugins/timespanview/timespanview.h
@@ -16,8 +16,8 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 */
-#ifndef KORG_PROJECTVIEW_H
-#define KORG_PROJECTVIEW_H
+#ifndef KORG_TIMESPANVIEW_H
+#define KORG_TIMESPANVIEW_H
 // $Id$
 
 #include <korganizer/part.h>
