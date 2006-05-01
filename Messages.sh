75fb25006a4456ab3d76cb83a13c8067588515ee
Stephan Kulow <coolo@kde.org>
2006-05-01 19:53:01 +0000

    moved messages targets
    svn path=/trunk/KDE/kdepim/kontact/plugins/; revision=536312

---
+++ Messages.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+$PREPARETIPS > tips.cpp
+$EXTRACTRC `find . -name "*.rc" -o -name "*.ui" -o -name "*.kcfg"` >> rc.cpp || exit 11
+$XGETTEXT `find . -name "*.cpp" -o -name "*.h"` -o $podir/korganizer.pot
+rm -f tips.cpp
