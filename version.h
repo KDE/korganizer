49e4e307a8013741887c061ae3f9b820f8440435
Bram Schoenmakers <bramschoenmakers@kde.nl>
2006-05-19 10:30:49 +0000

    SVN_SILENT: Version bump
    svn path=/branches/KDE/3.5/kdepim/; revision=542439

--- version.h
+++ version.h
@@ -33,14 +33,14 @@
   z is the minor release number.
 
   "x.y.z" follow the kdelibs version KOrganizer is released with.
-  
+
   If "z" is 0, it the version is "x.y"
-  
+
   "build" is empty for final versions. For development versions "build" is
   something like "pre", "alpha1", "alpha2", "beta1", "beta2", "rc1", "rc2".
 
   Examples in chronological order:
-  
+
     3.0
     3.0.1
     3.1 alpha1
@@ -53,6 +53,6 @@
     3.2 alpha1
 */
 
-static const char korgVersion[] = "3.5";
+static const char korgVersion[] = "3.5.3";
 
 #endif
