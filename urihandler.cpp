4231bad62a6d33c38e7ba76f2847ef8a6510083f
Till Adam <adam@kde.org>
2006-10-17 06:58:28 +0000

    Here too.
    svn path=/branches/kdepim/proko2/kdepim/; revision=596312

--- urihandler.cpp
+++ urihandler.cpp
@@ -53,7 +53,7 @@ bool UriHandler::process( const QString &uri )
       kmailIface.showMail( serialNumber, messageId );
       return true;
     }
-  } else if ( uri.toUpper().startsWith( "MAILTO:" ) ) {
+  } else if ( uri.upper().startsWith( "MAILTO:" ) ) {
     KApplication::kApplication()->invokeMailer( uri.mid(7), QString::null );
     return true;
   } else if ( uri.startsWith( "uid:" ) ) {
