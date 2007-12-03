9e1709456abaa0ebc439591b7b18f5d7033c413e
Allen Winter <winter@kde.org>
2007-12-03 14:54:03 +0000

    fix up a messy i18n string warnings-- coding style
    svn path=/trunk/KDE/kdepim/; revision=744416

--- kotimespentview.cpp
+++ kotimespentview.cpp
@@ -22,126 +22,126 @@
   without including the source code for Qt in the source distribution.
 */
 
-#include <QDate>
-#include <QMap>
-#include <QBoxLayout>
-#include <QPainter>
-#include <QPainterPath>
-
+#include "kotimespentview.h"
 #include "koglobals.h"
 #include "koprefs.h"
 
 #include <kcal/calendar.h>
 #include <kcal/event.h>
 
-#include "kotimespentview.h"
+#include <QDate>
+#include <QMap>
+#include <QBoxLayout>
+#include <QPainter>
+#include <QPainterPath>
 
-class TimeSpentWidget : public QWidget {
+class TimeSpentWidget : public QWidget
+{
+  public:
+    TimeSpentWidget( KOTimeSpentView *parent )
+      : QWidget( parent ), mTimeSpentView( parent ) {}
+    ~TimeSpentWidget() {}
 
-public:
-  TimeSpentWidget( KOTimeSpentView *parent ) : 
-    QWidget( parent ),
-    mTimeSpentView( parent ) {};
-  ~TimeSpentWidget() {};
+    void paintEvent( QPaintEvent *e )
+    {
+      QPainter p( this );
+      p.fillRect( e->rect(), Qt::white );
+      int margin = 10;
+      int y = 90;
 
-  void paintEvent(QPaintEvent *e) 
-  {   
-    QPainter p( this );
-    p.fillRect( e->rect(), Qt::white );
-    int margin = 10;
-    const int textlength = 10 * margin; //fixme bounding rect
-    int y = 90;
+      p.fillRect( QRect( 5, 5, width(), 35 ), QColor( 54, 121, 173 ) ); // sync with kowhatsnextview
+      QPen oldPen = p.pen();
+      QFont oldFont = p.font();
+      QFont font = p.font();
+      font.setPixelSize( 25 );
+      font.setBold( true );
+      p.setFont( font );
+      p.setPen( QColor( Qt::white ) );
+      p.drawText( QPoint( 25, 35 ), i18n( "Time Tracker" ) );
+      p.setPen( oldPen );
+      p.setFont( oldFont );
 
-    p.fillRect( QRect( 5, 5, width(), 35 ), QColor( 54, 121, 173 ) /* keep in sync with kowhatsnextview */ );
-    QPen oldPen = p.pen();
-    QFont oldFont = p.font();
-    QFont font = p.font();
-    font.setPixelSize( 25 );
-    font.setBold( true );
-    p.setFont( font );
-    p.setPen( QColor( Qt::white ) );
-    p.drawText( QPoint( 25, 35 ), i18n( "Time Tracker" ) );
-    p.setPen( oldPen );
-    p.setFont( oldFont );
-    
-    QString dateText;
-    if ( mTimeSpentView->mStartDate.daysTo( mTimeSpentView->mEndDate ) < 1 ) {
-      dateText = KGlobal::locale()->formatDate( mTimeSpentView->mStartDate );
-    } else {
-      dateText = i18nc("Date from - to", "%1 - %2",
-		     KGlobal::locale()->formatDate( mTimeSpentView->mStartDate ) ,
-		     KGlobal::locale()->formatDate( mTimeSpentView->mEndDate ) );
-    }
-    font.setPixelSize( 20 );
-    font.setBold( true );
-    p.setFont( font );
-    p.drawText( QPoint( margin, 60 ), dateText );
-    p.setPen( oldPen );
-    p.setFont( oldFont );
-    
-
-
-    QMap<QString, int> secondsSpent;
-    
-    int total = 0;
-    foreach( Event *e, mEventList ) {
-      if ( e->categories().count() ) {
-	foreach( QString s, e->categories() ) {
-	  secondsSpent[ s ] += e->dtStart().secsTo( e->dtEnd() );
-	}
+      QString dateText;
+      if ( mTimeSpentView->mStartDate.daysTo( mTimeSpentView->mEndDate ) < 1 ) {
+        dateText = KGlobal::locale()->formatDate( mTimeSpentView->mStartDate );
       } else {
-	secondsSpent[ i18n("No category") ] += e->dtStart().secsTo( e->dtEnd() );
+        dateText = i18nc( "Date from - to", "%1 - %2",
+                          KGlobal::locale()->formatDate( mTimeSpentView->mStartDate ),
+                          KGlobal::locale()->formatDate( mTimeSpentView->mEndDate ) );
       }
-      
-      total += e->dtStart().secsTo( e->dtEnd() );
+      font.setPixelSize( 20 );
+      font.setBold( true );
+      p.setFont( font );
+      p.drawText( QPoint( margin, 60 ), dateText );
+      p.setPen( oldPen );
+      p.setFont( oldFont );
+
+      QMap<QString, int> secondsSpent;
+
+      int total = 0;
+      foreach ( Event *e, mEventList ) {
+        if ( e->categories().count() ) {
+          foreach ( QString s, e->categories() ) {
+            secondsSpent[ s ] += e->dtStart().secsTo( e->dtEnd() );
+          }
+        } else {
+          secondsSpent[ i18n( "No category" ) ] += e->dtStart().secsTo( e->dtEnd() );
+        }
+
+        total += e->dtStart().secsTo( e->dtEnd() );
+      }
+
+      QMapIterator<QString, int> i( secondsSpent );
+      QFontMetrics fm = p.fontMetrics();
+      int lineHeight = fm.boundingRect( "No category" ).height();
+      int totalLineHeight = lineHeight + 2; // vertical margin included
+
+      while ( i.hasNext() ) {
+        i.next();
+
+        // bar
+        QColor color = KOPrefs::instance()->categoryColor( i.key() );
+        int length =  static_cast<int>( ( (double) i.value() ) / total * ( width() - 3 * margin ) );
+        QPainterPath path( QPoint( margin, y ) );
+        path.lineTo( margin + length, y );
+        if ( length < margin ) {
+          path.lineTo( margin + length, y + lineHeight );
+        } else {
+          path.arcTo( QRect( margin + length, y, 2 * margin, lineHeight ), +90, -180 );
+        }
+        path.lineTo( margin, y + lineHeight );
+        path.closeSubpath();
+        p.setBrush( color );
+        p.drawPath( path );
+
+        // text
+        int totHr, perHr;
+        if ( total > 0 ) {
+          totHr = i.value() / ( 60 * 60 );
+          perHr = static_cast<int>( ( (double) i.value() * 100 / total ) );
+        } else {
+          totHr = 0;
+          perHr = 0;
+        }
+        p.drawText( QRect( margin + 2, y + 2, width() - 2 * margin, lineHeight ),
+                    i.key() + ": " +
+                    i18ncp( "number of hours spent", "%1 hour", "%1 hours", totHr ) +
+                    i18nc( "percent of hours spent", " (%1%)", perHr ) );
+        y += totalLineHeight;
+      }
     }
-    
-       
-    QMapIterator<QString, int> i( secondsSpent );
-    QFontMetrics fm = p.fontMetrics();
-    int lineHeight = fm.boundingRect( "No category" ).height();
-    int totalLineHeight = lineHeight + 2; // vertical margin included
-    
-    while ( i.hasNext() ) {
-      i.next();
 
-      // bar
-      QColor color = KOPrefs::instance()->categoryColor( i.key() );
-      int length =  ( (double) i.value() ) / total * ( width() - 3 * margin ); 
-      QPainterPath path( QPoint( margin, y ) );
-      path.lineTo( margin + length, y );
-      if ( length < margin ) {
-	path.lineTo( margin + length, y + lineHeight );
-      }
-      else {
-	path.arcTo( QRect( margin + length, y, 2 * margin, lineHeight ), +90, -180 );
-      }
-      path.lineTo( margin, y + lineHeight );
-      path.closeSubpath();
-      p.setBrush( color );
-      p.drawPath( path );
-
-      // text
-      p.drawText( QRect( margin + 2, y + 2, width() - 2 * margin, lineHeight ), i.key()
-		  + ": "
-		  + i18nc( "Example: 5 hours (80%)", "%1 hours (", QString::number( i.value() / (60 * 60) ) )
-		  + QString::number( (int)( (double) i.value() * 100 / total ) )
-		  + "% )" );
-      y += totalLineHeight;    
-    }
-  }
-
-  Event::List mEventList;
-  KOTimeSpentView *mTimeSpentView;
+    Event::List mEventList;
+    KOTimeSpentView *mTimeSpentView;
 };
 
 KOTimeSpentView::KOTimeSpentView( Calendar *calendar, QWidget *parent )
-  : KOrg::BaseView( calendar, parent)
+  : KOrg::BaseView( calendar, parent )
 {
-  mView = new TimeSpentWidget(this);
+  mView = new TimeSpentWidget( this );
 
-  QBoxLayout *topLayout = new QVBoxLayout(this);
-  topLayout->addWidget(mView);
+  QBoxLayout *topLayout = new QVBoxLayout( this );
+  topLayout->addWidget( mView );
 }
 
 KOTimeSpentView::~KOTimeSpentView()
@@ -164,9 +164,9 @@ void KOTimeSpentView::showIncidences( const Incidence::List & )
 {
 }
 
-void KOTimeSpentView::changeIncidenceDisplay(Incidence *, int action)
+void KOTimeSpentView::changeIncidenceDisplay( Incidence *, int action )
 {
-  switch(action) {
+  switch( action ) {
     case KOGlobals::INCIDENCEADDED:
     case KOGlobals::INCIDENCEEDITED:
     case KOGlobals::INCIDENCEDELETED:
@@ -176,7 +176,6 @@ void KOTimeSpentView::changeIncidenceDisplay(Incidence *, int action)
       break;
   }
 }
-
 
 void KOTimeSpentView::updateView()
 {
@@ -204,11 +203,11 @@ void KOTimeSpentView::updateView()
   */
   Event::List events;
   KDateTime::Spec timeSpec = KOPrefs::instance()->timeSpec();
-  for ( QDate date = mStartDate; date <= mEndDate; date = date.addDays( 1 ) )
-    events += calendar()->events(date, timeSpec,
-                                 EventSortStartDate, SortDirectionAscending);
+  for ( QDate date = mStartDate; date <= mEndDate; date = date.addDays( 1 ) ) {
+    events += calendar()->events( date, timeSpec,
+                                  EventSortStartDate, SortDirectionAscending );
+  }
 
-  
   mView->mEventList = events;
   mView->repaint();
 }
