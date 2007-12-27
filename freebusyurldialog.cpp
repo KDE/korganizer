a30a5e56e34e3d997a024ebb26a1bd061247ac1a
Allen Winter <winter@kde.org>
2007-12-27 16:31:49 +0000

    fix i18n message coding style
    svn path=/trunk/KDE/kdepim/; revision=753493

--- freebusyurldialog.cpp
+++ freebusyurldialog.cpp
@@ -1,25 +1,25 @@
 /*
-    This file is part of KOrganizer.
+  This file is part of KOrganizer.
 
-    Copyright (c) 2004 Cornelius Schumacher <schumacher@kde.org>
+  Copyright (c) 2004 Cornelius Schumacher <schumacher@kde.org>
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-    As a special exception, permission is given to link this program
-    with any edition of Qt, and distribute the resulting executable,
-    without including the source code for Qt in the source distribution.
+  As a special exception, permission is given to link this program
+  with any edition of Qt, and distribute the resulting executable,
+  without including the source code for Qt in the source distribution.
 */
 
 #include "freebusyurldialog.h"
@@ -34,7 +34,6 @@
 
 #include <QLayout>
 #include <QLabel>
-//Added by qt3to4:
 #include <QVBoxLayout>
 #include <QFrame>
 #include <QBoxLayout>
@@ -42,10 +41,10 @@
 FreeBusyUrlDialog::FreeBusyUrlDialog( KCal::Attendee *attendee, QWidget *parent )
   : KDialog( parent )
 {
-  QFrame *topFrame = new QFrame(this);
+  QFrame *topFrame = new QFrame( this );
   setMainWidget( topFrame );
   setModal( true );
-  setCaption( i18n("Edit Free/Busy Location") );
+  setCaption( i18n( "Edit Free/Busy Location" ) );
   setButtons( Ok|Cancel );
   setDefaultButton( Ok );
 
@@ -57,7 +56,7 @@ FreeBusyUrlDialog::FreeBusyUrlDialog( KCal::Attendee *attendee, QWidget *parent 
   topLayout->addWidget( mWidget );
 
   mWidget->loadConfig();
-  connect(this, SIGNAL(okClicked()), this, SLOT(slotOk()));
+  connect( this, SIGNAL(okClicked()), this, SLOT(slotOk()) );
 }
 
 void FreeBusyUrlDialog::slotOk()
@@ -66,16 +65,15 @@ void FreeBusyUrlDialog::slotOk()
   accept();
 }
 
-
 FreeBusyUrlWidget::FreeBusyUrlWidget( KCal::Attendee *attendee, QWidget *parent )
   : QWidget( parent ), mAttendee( attendee )
 {
   QBoxLayout *topLayout = new QVBoxLayout( this );
   topLayout->setSpacing( KDialog::spacingHint() );
 
-  QLabel *label = new QLabel(
-      i18n("Location of Free/Busy information for %1 <%2>:", //krazy:exclude=i18ncheckarg
-        mAttendee->name(), mAttendee->email() ), this );
+  QLabel *label =
+    new QLabel( i18n( "Location of Free/Busy information for %1 <placeholder>%2</placeholder>:",
+                     mAttendee->name(), mAttendee->email() ), this );
   topLayout->addWidget( label );
 
   mUrlEdit = new KLineEdit( this );
@@ -88,21 +86,18 @@ FreeBusyUrlWidget::~FreeBusyUrlWidget()
 
 void FreeBusyUrlWidget::loadConfig()
 {
-  kDebug(5850) <<"FreeBusyUrlWidget::loadConfig()";
+  kDebug(5850);
 
   QString url = KCal::FreeBusyUrlStore::self()->readUrl( mAttendee->email() );
-
   mUrlEdit->setText( url );
 }
 
 void FreeBusyUrlWidget::saveConfig()
 {
-  kDebug(5850) <<"FreeBusyUrlWidget::saveConfig()";
+  kDebug(5850);
 
   QString url = mUrlEdit->text();
-
   KCal::FreeBusyUrlStore::self()->writeUrl( mAttendee->email(), url );
-
   KCal::FreeBusyUrlStore::self()->sync();
 }
 
