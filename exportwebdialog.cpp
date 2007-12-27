72e32c44d26a43d0beb2f01b694fc77bc182c7d8
Allen Winter <winter@kde.org>
2007-12-27 16:39:28 +0000

    give context to a i18n message kDebug() message fix style cleaning
    svn path=/trunk/KDE/kdepim/; revision=753494

--- exportwebdialog.cpp
+++ exportwebdialog.cpp
@@ -63,17 +63,17 @@
 //        because we want custom buttons, a Tabbed dialog, and a different
 //        headline... Maybe we should try to achieve the same without code
 //        duplication.
-ExportWebDialog::ExportWebDialog( HTMLExportSettings *settings, QWidget *parent)
+ExportWebDialog::ExportWebDialog( HTMLExportSettings *settings, QWidget *parent )
   : KPageDialog( parent ),
     KPrefsWidManager( settings ), mSettings( settings )
 {
   setFaceType( Tabbed );
-  setCaption( i18n("Export Calendar as Web Page") );
+  setCaption( i18n( "Export Calendar as Web Page" ) );
   setButtons( Help|Default|User1|Cancel );
   setDefaultButton( User1 );
   setModal( false );
   showButtonSeparator( false );
-  setButtonText( User1, i18n("Export") );
+  setButtonText( User1, i18n( "Export" ) );
 
   setupGeneralPage();
   setupEventPage();
@@ -83,10 +83,10 @@ ExportWebDialog::ExportWebDialog( HTMLExportSettings *settings, QWidget *parent)
 //  setupFreeBusyPage();
 //  setupAdvancedPage();
 
-  connect( this, SIGNAL( user1Clicked() ), SLOT( slotOk() ) );
-  connect( this, SIGNAL( cancelClicked() ), SLOT( reject() ) );
-  connect( this, SIGNAL( defaultClicked()) , this, SLOT(slotDefault()));
-  connect(this,SIGNAL(applyClicked()),this,SLOT(slotApply()));
+  connect( this, SIGNAL(user1Clicked()), SLOT(slotOk()) );
+  connect( this, SIGNAL(cancelClicked()), SLOT(reject()) );
+  connect( this, SIGNAL(defaultClicked()), this, SLOT(slotDefault()) );
+  connect( this, SIGNAL(applyClicked()), this, SLOT(slotApply()) );
   readConfig();
 }
 
@@ -127,24 +127,26 @@ void ExportWebDialog::slotOk()
 
 void ExportWebDialog::slotDefault()
 {
-  kDebug(5850) <<"KPrefsDialog::slotDefault()";
+  kDebug(5850);
 
-  if (KMessageBox::warningContinueCancel(this,
-      i18n("You are about to set all preferences to default values. All "
-      "custom modifications will be lost."),i18n("Setting Default Preferences"),
-      KGuiItem(i18n("Reset to Defaults")))
-    == KMessageBox::Continue) setDefaults();
+  if ( KMessageBox::warningContinueCancel(
+         this,
+         i18n( "You are about to set all preferences to default values. "
+               "All custom modifications will be lost." ),
+         i18n( "Setting Default Preferences" ),
+      KGuiItem( i18n( "Reset to Defaults" ) ) ) == KMessageBox::Continue ) {
+    setDefaults();
+  }
 }
-
 
 void ExportWebDialog::setupGeneralPage()
 {
   mGeneralPage = new QFrame( this );
-  addPage( mGeneralPage, i18n("General") );
+  addPage( mGeneralPage, i18nc( "general settings for html export", "General" ) );
   QVBoxLayout *topLayout = new QVBoxLayout(mGeneralPage);
   topLayout->setSpacing(10);
 
-  QGroupBox *rangeGroup = new QGroupBox( i18n("Date Range"), mGeneralPage );
+  QGroupBox *rangeGroup = new QGroupBox( i18n( "Date Range" ), mGeneralPage );
   topLayout->addWidget( rangeGroup );
 
   QBoxLayout *rangeLayout = new QVBoxLayout( rangeGroup );
@@ -156,7 +158,7 @@ void ExportWebDialog::setupGeneralPage()
   rangeLayout->addWidget( dateEnd->label() );
   rangeLayout->addWidget( dateEnd->dateEdit() );
 
-  QGroupBox *typeGroup = new QGroupBox( i18n("View Type"), mGeneralPage );
+  QGroupBox *typeGroup = new QGroupBox( i18n( "View Type" ), mGeneralPage );
   topLayout->addWidget( typeGroup );
 
   QBoxLayout *typeLayout = new QVBoxLayout( typeGroup );
@@ -169,22 +171,22 @@ void ExportWebDialog::setupGeneralPage()
   typeLayout->addWidget(
     addWidBool( mSettings->excludeConfidentialItem() )->checkBox() );
 
-  QGroupBox *destGroup = new QGroupBox(i18n("Destination"), mGeneralPage );
-  topLayout->addWidget(destGroup);
+  QGroupBox *destGroup = new QGroupBox( i18n( "Destination" ), mGeneralPage );
+  topLayout->addWidget( destGroup );
 
   QBoxLayout *destLayout = new QHBoxLayout( destGroup );
 
   KPrefsWidPath *pathWid = addWidPath( mSettings->outputFileItem(),
                                        destGroup, "text/html", KFile::File );
-  connect( pathWid->urlRequester(), SIGNAL( textChanged( const QString & ) ),
-           SLOT( slotTextChanged( const QString & ) ) );
+  connect( pathWid->urlRequester(), SIGNAL(textChanged(const QString&)),
+           SLOT(slotTextChanged(const QString&)) );
   destLayout->addWidget( pathWid->label() );
   destLayout->addWidget( pathWid->urlRequester() );
 
   topLayout->addStretch( 1 );
 }
 
-void ExportWebDialog::slotTextChanged( const QString & _text)
+void ExportWebDialog::slotTextChanged( const QString &_text )
 {
     enableButton( User1, !_text.isEmpty() );
 }
@@ -192,7 +194,7 @@ void ExportWebDialog::slotTextChanged( const QString & _text)
 void ExportWebDialog::setupTodoPage()
 {
   mTodoPage = new QFrame( this );
-  addPage( mTodoPage, i18n("To-dos") );
+  addPage( mTodoPage, i18n( "To-dos" ) );
   QVBoxLayout *topLayout = new QVBoxLayout( mTodoPage );
   topLayout->setSpacing( 10 );
 
@@ -215,7 +217,7 @@ void ExportWebDialog::setupTodoPage()
 void ExportWebDialog::setupEventPage()
 {
   mEventPage = new QFrame( this );
-  addPage( mEventPage, i18n("Events") );
+  addPage( mEventPage, i18n( "Events" ) );
   QVBoxLayout *topLayout = new QVBoxLayout( mEventPage );
   topLayout->setSpacing( 10 );
 
