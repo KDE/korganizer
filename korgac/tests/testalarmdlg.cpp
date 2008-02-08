44bf84469b68b372a0a1836de515a7a4d78ba0c9
Allen Winter <winter@kde.org>
2008-02-08 03:21:42 +0000

    minor coding style fixes SVN_SILENT:
    svn path=/trunk/KDE/kdepim/; revision=772233

--- korgac/tests/testalarmdlg.cpp
+++ korgac/tests/testalarmdlg.cpp
@@ -1,25 +1,25 @@
 /*
-    This file is part of KOrganizer.
+  This file is part of KOrganizer.
 
-    Copyright (c) 2002 Cornelius Schumacher <schumacher@kde.org>
+  Copyright (c) 2002 Cornelius Schumacher <schumacher@kde.org>
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-    As a special exception, permission is given to link this program
-    with any edition of Qt, and distribute the resulting executable,
-    without including the source code for Qt in the source distribution.
+  As a special exception, permission is given to link this program
+  with any edition of Qt, and distribute the resulting executable,
+  without including the source code for Qt in the source distribution.
 */
 
 #include "alarmdialog.h"
@@ -35,11 +35,10 @@ using namespace KCal;
 
 #include <QWidget>
 
-
-int main(int argc,char **argv)
+int main( int argc, char **argv )
 {
-  KAboutData aboutData("testkabc", 0,ki18n("TestKabc"),"0.1");
-  KCmdLineArgs::init(argc,argv,&aboutData);
+  KAboutData aboutData( "testkabc", 0, ki18n( "TestKabc" ), "0.1" );
+  KCmdLineArgs::init( argc, argv, &aboutData );
 
   KApplication app;
 
