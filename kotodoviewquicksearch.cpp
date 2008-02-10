27afc1e6577d4d0a8f62ec61159bff5ffb9af5b2
Allen Winter <winter@kde.org>
2008-02-10 15:58:34 +0000

    KUIT
    svn path=/trunk/KDE/kdepim/; revision=773228

--- kotodoviewquicksearch.cpp
+++ kotodoviewquicksearch.cpp
@@ -1,47 +1,44 @@
 /*
-    This file is part of KOrganizer.
+  This file is part of KOrganizer.
 
-    Copyright (c) 2004 Till Adam <adam@kde.org>
-    Copyright (c) 2005 Rafal Rzepecki <divide@users.sourceforge.net>
+  Copyright (c) 2004 Till Adam <adam@kde.org>
+  Copyright (c) 2005 Rafal Rzepecki <divide@users.sourceforge.net>
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
 
-    As a special exception, permission is given to link this program
-    with any edition of Qt, and distribute the resulting executable,
-    without including the source code for Qt in the source distribution.
+  As a special exception, permission is given to link this program
+  with any edition of Qt, and distribute the resulting executable,
+  without including the source code for Qt in the source distribution.
 */
 
-
 #include "kotodoviewquicksearch.h"
-
 #include "kotodoviewitem.h"
 #include "kotodoview.h"
+#include "koprefs.h"
+#include "korganizer/mainwindow.h"
+
+#include <libkdepim/categoryhierarchyreader.h>
 
 #include <kcal/calendar.h>
 #include <kcal/calfilter.h>
-#include <libkdepim/categoryhierarchyreader.h>
 
-#include <korganizer/mainwindow.h>
-
-#include "koprefs.h"
-
-#include <kaction.h>
+#include <KAction>
 #include <k3listviewsearchline.h>
 #include <KComboBox>
-#include <kicon.h>
+#include <KIcon>
 
 #include <QLayout>
 #include <QLabel>
@@ -144,7 +141,7 @@ void KOTodoListViewQuickSearch::fillCategories()
   }
 
   CategoryHierarchyReaderQComboBox( mCategoryCombo ).read( categories );
-  mCategoryCombo->insertItem( 0, i18n( "Any category" ) );
+  mCategoryCombo->insertItem( 0, i18nc( "@item:inlistbox", "Any category" ) );
 
   categoryList.resize( categories.count() );
   qCopy( categories.begin(), categories.end(), categoryList.begin() );
