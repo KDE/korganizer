cbd8eeb494b48a86e2851a8764d6691436a363a4
Kevin Ottens <ervin@kde.org>
2008-02-21 16:59:18 +0000

    Prokde35-s Item 61: Extended free/busy list support (kdepim part)
    Merged revisions 744087-744110 via svnmerge from
    svn+ssh://ervin@svn.kde.org/home/kde/branches/kdepim/enterprise/kdepim
    
    ................
      r744087 | vkrause | 2007-12-02 18:08:00 +0100 (dim, 02 déc 2007) | 2 lines
    
      Support for reading extended free/busy lists.
    ................
      r744092 | vkrause | 2007-12-02 18:19:34 +0100 (dim, 02 déc 2007) | 2 lines
    
      Show extended free/busy information.
    ................
      r744110 | vkrause | 2007-12-02 18:47:07 +0100 (dim, 02 déc 2007) | 9 lines
    
      Merged revisions 689908 via svnmerge from
      https://vkrause@svn.kde.org/home/kde/branches/KDE/3.5/kdepim
    
      ........
        r689908 | bvirlet | 2007-07-19 15:23:54 +0200 (Thu, 19 Jul 2007) | 3 lines
    
        Remove these signal emissions already done in the subwidget.
      ........
    ................
    
    
    svn path=/trunk/KDE/kdepim/; revision=777806

--- koeditorfreebusy.cpp
+++ koeditorfreebusy.cpp
@@ -177,6 +177,13 @@ void FreeBusyItem::setFreeBusyPeriods( FreeBusy *fb )
       newSubItem->setStartTime( (*it).start().toTimeSpec( timeSpec ).dateTime() );
       newSubItem->setEndTime( (*it).end().toTimeSpec( timeSpec ).dateTime() );
       newSubItem->setColors( Qt::red, Qt::red, Qt::red );
+      QString toolTip;
+      if ( !(*it).summary().isEmpty() )
+        toolTip += "<b>" + (*it).summary() + "</b><br/>";
+      if ( !(*it).location().isEmpty() )
+        toolTip += i18n( "Location: %1" ).arg( (*it).location() );
+      if ( !toolTip.isEmpty() )
+        newSubItem->setTooltipText( toolTip );
     }
     setFreeBusy( fb );
     setShowNoInformation( false );
