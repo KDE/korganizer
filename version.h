0722d96365d869586d7825db9c727ba6b9600d50
Pradeepto Bhattacharya <pradeepto@kde.org>
2008-04-04 10:16:13 +0000

    Update version numbers for - Happy Birthday Till Adam and Happy Anniversary Jesper release
    svn path=/branches/kdepim/enterprise/kdepim/; revision=793539

--- version.h
+++ version.h
@@ -53,6 +53,6 @@
     3.2 alpha1
 */
 
-static const char korgVersion[] = "3.5.9 (enterprise 0.20080329.791441)";
+static const char korgVersion[] = "3.5.9 (enterprise 0.20080404.793538)";
 
 #endif
