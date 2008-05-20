04faabf0157fbb78a4b2dbe0ce596401185363fa
Pradeepto Bhattacharya <pradeepto@kde.org>
2008-05-20 13:05:40 +0000

    SVN_SILENT Ich bin ein Berliner Release.
    svn path=/branches/kdepim/enterprise/kdepim/; revision=810305

--- version.h
+++ version.h
@@ -53,6 +53,6 @@
     3.2 alpha1
 */
 
-static const char korgVersion[] = "3.5.9 (enterprise 0.20080509.805825)";
+static const char korgVersion[] = "3.5.9 (enterprise 0.20080520.810303)";
 
 #endif
