bbe123ea32227fe276a45f5f6c06b4cdf9a0ad85
Pradeepto Bhattacharya <pradeepto@kde.org>
2008-06-18 02:09:44 +0000

    ouch forgot to commit the version bumps yesterday...sigh :(
    svn path=/branches/kdepim/enterprise/kdepim/; revision=821649

--- version.h
+++ version.h
@@ -53,6 +53,6 @@
     3.2 alpha1
 */
 
-static const char korgVersion[] = "3.5.9 (enterprise35 0.20080617.821371)";
+static const char korgVersion[] = "3.5.9 (enterprise35 0.20080617.821379)";
 
 #endif
