451c408585d0d3b12ee7293bd59ae272cae40114
Allen Winter <winter@kde.org>
2008-06-25 15:36:55 +0000

    add a comment about the Quit not quitting fix SVN_SILENT:
    svn path=/trunk/KDE/kdepim/; revision=824294

--- korgac/korgacmain.cpp
+++ korgac/korgacmain.cpp
@@ -37,6 +37,7 @@ class ReminderDaemonApp : public KUniqueApplication
   public:
     ReminderDaemonApp() : mClient( 0 )
     {
+      // ensure the Quit dialog's Cancel reponse does not close the app
       setQuitOnLastWindowClosed( false );
     }
 
