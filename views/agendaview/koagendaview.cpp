624786a1fdde860268817391ca21ea7acc0612c9
Allen Winter <winter@kde.org>
2008-08-27 17:38:50 +0000

    E3 Port SVN commit 852422 by vkrause:
    Update the view correctly if an agenda item move was aborted by
    cancelling one of the subsequent dialogs.
    
    Kolab issue 2514
    
    svn path=/trunk/KDE/kdepim/; revision=853416

--- views/agendaview/koagendaview.cpp
+++ views/agendaview/koagendaview.cpp
@@ -1047,9 +1047,17 @@ void KOAgendaView::updateEventDates( KOAgendaItem *item )
 
   item->setToolTip( IncidenceFormatter::toolTipString( incidence ) );
 
-  mChanger->changeIncidence( oldIncidence, incidence );
+  const bool result = mChanger->changeIncidence( oldIncidence, incidence );
   mChanger->endChange(incidence);
   delete oldIncidence;
+
+  // Update the view correctly if an agenda item move was aborted by
+  // cancelling one of the subsequent dialogs.
+  if ( !result ) {
+    mPendingChanges = true;
+    QTimer::singleShot( 0, this, SLOT(updateView()) );
+    return;
+  }
 
   // don't update the agenda as the item already has the correct coordinates.
   // an update would delete the current item and recreate it, but we are still
