209e25aaa2457fbec11fef30995a6ae732546abc
Volker Krause <vkrause@kde.org>
2008-10-24 15:11:25 +0000

    Merged revisions 871207,871216 via svnmerge from https://vkrause@svn.kde.org/home/kde/branches/kdepim/enterprise/kdepim
    ........
      r871207 | pradeepto | 2008-10-14 08:59:45 +0200 (Tue, 14 Oct 2008) | 2 lines
      
       -- kolab/issue2702 Make sure next attendee is selected after deletion of an attendee. (if applicable, of course)
    ........
      r871216 | pradeepto | 2008-10-14 09:27:27 +0200 (Tue, 14 Oct 2008) | 2 lines
      
       -- kolab/issue2702 - Part 2 - for ToDo Editor now.
    ........
    
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=875481

--- koeditorfreebusy.cpp
+++ koeditorfreebusy.cpp
@@ -838,6 +838,12 @@ void KOEditorFreeBusy::removeAttendee()
     return;
   }
 
+  FreeBusyItem *nextSelectedItem = static_cast<FreeBusyItem*>( item->nextSibling() );
+  if( mGanttView->childCount() == 1 )
+      nextSelectedItem = 0;
+  if( mGanttView->childCount() > 1 && item == mGanttView->lastItem() )
+      nextSelectedItem = static_cast<FreeBusyItem*>(  mGanttView->firstChild() );
+
   Attendee *delA = new Attendee( item->attendee()->name(), item->attendee()->email(),
                                  item->attendee()->RSVP(), item->attendee()->status(),
                                  item->attendee()->role(), item->attendee()->uid() );
@@ -845,6 +851,8 @@ void KOEditorFreeBusy::removeAttendee()
   delete item;
 
   updateStatusSummary();
+  if( nextSelectedItem )
+      mGanttView->setSelected( nextSelectedItem, true );
   updateAttendeeInput();
   emit updateAttendeeSummary( mGanttView->childCount() );
 }
