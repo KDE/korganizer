8cf4d97f4d9dc9219198d37d0e11fef8f93dc094
Pradeepto Bhattacharya <pradeepto@kde.org>
2008-10-27 04:07:02 +0000

    -- Update version numbers.
    svn path=/branches/kdepim/enterprise/kdepim/; revision=876310

--- version.h
+++ version.h
@@ -53,6 +53,6 @@
     3.2 alpha1
 */
 
-static const char korgVersion[] = "3.5.9 (enterprise35 0.20081017.872568)";
+static const char korgVersion[] = "3.5.9 (enterprise35 0.20081027.876309)";
 
 #endif
