5629094b72f134856205bdbf546c3dad775756cd
Allen Winter <winter@kde.org>
2009-01-03 16:27:24 +0000

    tell Krazy to skip the qclasses check (since we are subclassing from QComboBox)
    svn path=/trunk/KDE/kdepim/; revision=905049

--- views/todoview/kcheckcombobox.h
+++ views/todoview/kcheckcombobox.h
@@ -21,6 +21,7 @@
   with any edition of Qt, and distribute the resulting executable,
   without including the source code for Qt in the source distribution.
 */
+//krazy:excludeall=qclasses because we subclass from QComboBox
 
 #ifndef KCHECKCOMBOBOX_H
 #define KCHECKCOMBOBOX_H
