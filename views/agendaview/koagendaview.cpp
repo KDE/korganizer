29cfc8df98da493f36852649736775f5fe6f77f4
Allen Winter <winter@kde.org>
2009-01-20 00:47:32 +0000

    backport SVN commit 913872 by winterz:
    Use KWordWrap to force holiday strings to fix within the day label box.
    If the string doesn't fit, it is truncated with elipsis.
    
    This should fix some problems with day header labels not properly aligning.
    
    
    svn path=/branches/KDE/4.2/kdepim/; revision=913874

--- views/agendaview/koagendaview.cpp
+++ views/agendaview/koagendaview.cpp
@@ -58,6 +58,7 @@
 #include <ksystemtimezone.h>
 #include <kpushbutton.h>
 #include <kcombobox.h>
+#include <kwordwrap.h>
 
 #include <QLabel>
 #include <QFrame>
@@ -462,6 +463,8 @@ void KOAgendaView::createDayLabels()
   delete mDayLabels;
   delete mBottomDayLabels;
 
+  QFontMetrics fm = fontMetrics();
+
   mDayLabels = new QFrame ( mDayLabelsFrame );
   mDayLabelsFrame->setStretchFactor( mDayLabels, 1 );
   mLayoutDayLabels = new QHBoxLayout( mDayLabels );
@@ -542,9 +545,10 @@ void KOAgendaView::createDayLabels()
     QStringList texts = KOGlobals::self()->holiday( date );
     QStringList::ConstIterator textit = texts.constBegin();
     for ( ; textit != texts.constEnd(); ++textit ) {
-      // use a KOAlternateLabel so when the text doesn't fit any more a tooltip is used
+      // Compute a small version of the holiday string for KOAlternateLabel
+      KWordWrap *ww = KWordWrap::formatText( fm, dayLabelBox->rect(), 0, (*textit), -1 );
       KOAlternateLabel *label =
-        new KOAlternateLabel( (*textit), (*textit), QString(), dayLabelBox );
+        new KOAlternateLabel( ww->truncatedString(), (*textit), (*textit), dayLabelBox );
       label->setMinimumWidth( 1 );
       label->setAlignment( Qt::AlignCenter );
     }
