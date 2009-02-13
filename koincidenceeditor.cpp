1594bdab5f4dbcb3673976f0b809f9f485a4c6be
Laurent Montel <montel@kde.org>
2009-02-13 10:40:25 +0000

    Fix mem leak
    svn path=/trunk/KDE/kdepim/; revision=925439

--- koincidenceeditor.cpp
+++ koincidenceeditor.cpp
@@ -77,7 +77,6 @@ KOIncidenceEditor::KOIncidenceEditor( const QString &caption,
   } else {
     setButtonText( Default, i18n( "Manage &Templates..." ) );
   }
-
   connect( this, SIGNAL(defaultClicked()), SLOT(slotManageTemplates()) );
   connect( this, SIGNAL(finished()), SLOT(delayedDestruct()) );
 }
@@ -166,7 +165,6 @@ void KOIncidenceEditor::cancelRemovedAttendees( Incidence *incidence )
 void KOIncidenceEditor::slotManageTemplates()
 {
   QString tp = type();
-
   TemplateManagementDialog * const d = new TemplateManagementDialog( this, templates() );
   connect( d, SIGNAL( loadTemplate( const QString& ) ),
            this, SLOT( slotLoadTemplate( const QString& ) ) );
@@ -175,7 +173,7 @@ void KOIncidenceEditor::slotManageTemplates()
   connect( d, SIGNAL( saveTemplate( const QString& ) ),
            this, SLOT( slotSaveTemplate( const QString& ) ) );
   d->exec();
-  return;
+  delete d;
 }
 
 void KOIncidenceEditor::saveAsTemplate( Incidence *incidence, const QString &templateName )
