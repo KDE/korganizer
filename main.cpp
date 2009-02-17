f246bdbcf5dc16e5537e4c78a348114a4c118229
Luk Tinkl <lukas@kde.org>
2009-02-17 15:42:50 +0000

    load the needed catalogs
    svn path=/trunk/KDE/kdepim/; revision=927465

--- main.cpp
+++ main.cpp
@@ -52,6 +52,7 @@ int main ( int argc, char **argv )
   KGlobal::locale()->insertCatalog( "libkcal" );
   KGlobal::locale()->insertCatalog( "libkdepim" );
   KGlobal::locale()->insertCatalog( "kdgantt1" );
+  KGlobal::locale()->insertCatalog( "libkresources" );
 
   if ( app.isSessionRestored() ) {
     RESTORE( KOrganizer )
