8089a179bd35691c6bd4ccdeebbcd17d807335d0
Sergio Luis Martins <iamsergio@gmail.com>
2009-02-21 01:27:44 +0000

    Correct tooltip for the "Manage Templates" button. Don't backport.
    BUG: 184738
    
    svn path=/trunk/KDE/kdepim/; revision=929303

--- koincidenceeditor.cpp
+++ koincidenceeditor.cpp
@@ -76,6 +76,7 @@ KOIncidenceEditor::KOIncidenceEditor( const QString &caption,
     showButton( Default, false );
   } else {
     setButtonText( Default, i18n( "Manage &Templates..." ) );
+    setButtonToolTip( Default, i18n( "Apply or create templates for this item" ) );
   }
   connect( this, SIGNAL(defaultClicked()), SLOT(slotManageTemplates()) );
   connect( this, SIGNAL(finished()), SLOT(delayedDestruct()) );
