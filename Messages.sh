f3936ac856100cad3d199ac729bd75f73c959bc9
Laurent Montel <montel@kde.org>
2009-02-23 12:41:19 +0000

    Remove it
    svn path=/trunk/KDE/kdepim/; revision=930414

--- Messages.sh
+++ Messages.sh
@@ -3,3 +3,4 @@ $PREPARETIPS > tips.cpp
 $EXTRACTRC `find . -name "*.rc" -o -name "*.ui" -o -name "*.kcfg"` >> rc.cpp || exit 11
 $XGETTEXT `find . -name "*.cpp" -o -name "*.h"` -o $podir/korganizer.pot
 rm -f tips.cpp
+rm -f rc.cpp
