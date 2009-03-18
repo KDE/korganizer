0943878464a956da12165e5dc6a3ecdea69d95fe
Thomas McGuire <mcguire@kde.org>
2009-03-18 12:09:43 +0000

    Merged revisions 939616 via svnmerge from svn+ssh://tmcguire@svn.kde.org/home/kde/branches/KDE/4.2/kdepim
    ........
      r939616 | winterz | 2009-03-15 13:27:02 +0100 (Sun, 15 Mar 2009) | 5 lines
      
      backport SVN commit 939236 by smartins:
      
      Forgot to add this. If the Occurrence is the only one (isFirst and isLast) a confirmation message should still be presented.
    ........
    
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=940852

--- calendarview.cpp
+++ calendarview.cpp
@@ -2175,6 +2175,8 @@ void CalendarView::deleteIncidence( Incidence *incidence, bool force )
             KGuiItem( i18n( "Delete C&urrent" ) ),
             itemFuture,
             KGuiItem( i18n( "Delete &All" ) ) );
+        } else {
+          km = msgItemDelete( incidence );
         }
       }
     }
