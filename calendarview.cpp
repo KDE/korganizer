b9d85b2f608ef57491d262742534be39bdefab0c
Thomas McGuire <mcguire@kde.org>
2009-03-25 15:37:02 +0000

    Merged revisions 942101 via svnmerge from svn+ssh://tmcguire@svn.kde.org/home/kde/branches/KDE/4.2/kdepim
    ........
      r942101 | smartins | 2009-03-21 04:19:40 +0100 (Sat, 21 Mar 2009) | 4 lines
      
      Backport r942100 by smartins from trunk to the 4.2 branch:
      
      Pasting journals now works (cutting was already possible).
    ........
    
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=944484

--- calendarview.cpp
+++ calendarview.cpp
@@ -931,6 +931,8 @@ void CalendarView::edit_paste()
       pastedTodo->setRelatedTo( _selectedTodo );
     }
     mChanger->addIncidence( pastedTodo, this );
+  } else if ( pastedIncidence->type() == "Journal" ) {
+    mChanger->addIncidence( pastedIncidence, this );
   }
 }
 
