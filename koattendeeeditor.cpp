0f5c9c9cb23016420c9fdb080a75b37301902b4f
Laurent Montel <montel@kde.org>
2009-04-07 13:30:12 +0000

    Backport: Fix enable disable "Response request" when it's myself
    svn path=/branches/KDE/4.2/kdepim/; revision=950586

--- koattendeeeditor.cpp
+++ koattendeeeditor.cpp
@@ -410,6 +410,9 @@ void KOAttendeeEditor::fillAttendeeInput( KCal::Attendee *a )
       mDelegateLabel->setText( i18n( "Not delegated" ) );
     }
   }
+  if( myself )
+    mRsvpButton->setEnabled( false );
+
 }
 
 void KOAttendeeEditor::updateAttendeeInput()
