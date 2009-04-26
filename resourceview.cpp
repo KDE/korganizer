8cdd1508d4734fbf50cac381fec1a6a7ec25a22f
Sergio Luis Martins <iamsergio@gmail.com>
2009-04-26 21:18:33 +0000

    If the user used RMB in the blank space (outside all item) the context menu was acting upon the previously selected item. If the user had for example an akonadi resource selected it was impossible to add another top level resource without restarting korg, it was only possible to add subresources.
    Now, RMB in the blank space will ignore selected items and always show the generic add resource dialog.
    
    svn path=/trunk/KDE/kdepim/; revision=959710

--- resourceview.cpp
+++ resourceview.cpp
@@ -255,6 +255,8 @@ ResourceView::ResourceView( KCal::CalendarResources *calendar, QWidget *parent )
   mListView->header()->hide();
   topLayout->addWidget( mListView );
 
+  mSelectedParent = 0;
+
   connect( mListView, SIGNAL(itemDoubleClicked(QTreeWidgetItem *,int)),
            SLOT(editResource()) );
   connect( mListView, SIGNAL(itemClicked(QTreeWidgetItem *,int)),
@@ -302,7 +304,7 @@ ResourceView::ResourceView( KCal::CalendarResources *calendar, QWidget *parent )
   mDeleteButton->setDisabled( true );
   mEditButton->setDisabled( true );
 
-  connect( mAddButton, SIGNAL( clicked() ), SLOT( addResource() ) );
+  connect( mAddButton, SIGNAL( clicked() ), SLOT( slotAddButtonClicked() ) );
   connect( mDeleteButton, SIGNAL( clicked() ), SLOT( removeResource() ) );
   connect( mEditButton, SIGNAL( clicked() ), SLOT( editResource() ) );
 
@@ -338,14 +340,23 @@ void ResourceView::emitResourcesChanged()
   emit resourcesChanged();
 }
 
+void ResourceView::slotAddButtonClicked()
+{
+  if ( !mListView->selectedItems().isEmpty() ) {
+    mSelectedParent = static_cast<ResourceItem*>( mListView->selectedItems().first() );
+  } else {
+    mSelectedParent = 0;
+  }
+
+  addResource();
+}
+
 void ResourceView::addResource()
 {
   bool ok = false;
   KCal::CalendarResourceManager *manager = mCalendar->resourceManager();
-  ResourceItem *i = 0;
-  if ( !mListView->selectedItems().isEmpty() ) {
-    i = static_cast<ResourceItem*>( mListView->selectedItems().first() );
-  }
+  ResourceItem *i = mSelectedParent;
+
   if ( i && ( i->isSubresource() || i->resource()->canHaveSubresources() ) ) {
     const QString folderName =
       KInputDialog::getText( i18n( "Add Subresource" ),
@@ -638,12 +649,14 @@ void ResourceView::showContextMenu( const QPoint &pos )
     QMenu *menu = new QMenu( this );
     menu->addAction( i18n( "&Add Resource..." ), this, SLOT(addResource()) );
     menu->popup( mapToGlobal( pos ) );
+    mSelectedParent = 0;
 
     return;
   }
 
   KCal::CalendarResourceManager *manager = mCalendar->resourceManager();
   ResourceItem *item = static_cast<ResourceItem *>( i );
+  mSelectedParent = item;
 
   QMenu *menu = new QMenu( this );
   connect( menu, SIGNAL(aboutToHide()), menu, SLOT(deleteLater()) );
