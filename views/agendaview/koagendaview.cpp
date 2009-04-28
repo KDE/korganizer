bb40824b832d118f53bf7a2b2131ce4c3a538f18
Andr Wbbeking <woebbeking@kde.org>
2009-04-28 22:13:28 +0000

    createDayLabels(): don't leak KWordWrap
    svn path=/branches/KDE/4.2/kdepim/; revision=960724

--- views/agendaview/koagendaview.cpp
+++ views/agendaview/koagendaview.cpp
@@ -545,11 +545,12 @@ void KOAgendaView::createDayLabels()
     QStringList::ConstIterator textit = texts.constBegin();
     for ( ; textit != texts.constEnd(); ++textit ) {
       // Compute a small version of the holiday string for KOAlternateLabel
-      KWordWrap *ww = KWordWrap::formatText( fm, dayLabelBox->rect(), 0, (*textit), -1 );
+      const KWordWrap *ww = KWordWrap::formatText( fm, dayLabelBox->rect(), 0, (*textit), -1 );
       KOAlternateLabel *label =
         new KOAlternateLabel( ww->truncatedString(), (*textit), (*textit), dayLabelBox );
       label->setMinimumWidth( 1 );
       label->setAlignment( Qt::AlignCenter );
+      delete ww;
     }
 
 #ifndef KORG_NOPLUGINS
