af921afbede5ffd74455e0c9e7594ec7b2e5179f
Thomas McGuire <mcguire@kde.org>
2009-04-30 15:24:40 +0000

    Merged revisions 961309 via svnmerge from svn+ssh://tmcguire@svn.kde.org/home/kde/branches/KDE/4.2/kdepim
    ........
      r961309 | woebbe | 2009-04-29 23:36:51 +0200 (Wed, 29 Apr 2009) | 1 line
      
      also free mHistory in dtor
    ........
    
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=961730

--- calendarview.cpp
+++ calendarview.cpp
@@ -261,7 +261,7 @@ CalendarView::~CalendarView()
   delete mDialogManager;
   delete mViewManager;
   delete mEventViewer;
-  kDebug() << "done";
+  delete mHistory;
 }
 
 void CalendarView::setCalendar( Calendar *cal )
@@ -274,9 +274,7 @@ void CalendarView::setCalendar( Calendar *cal )
   connect( mHistory, SIGNAL(undone()), SLOT(updateView()) );
   connect( mHistory, SIGNAL(redone()), SLOT(updateView()) );
 
-  if ( mChanger ) {
-    delete mChanger;
-  }
+  delete mChanger;
   setIncidenceChanger( new IncidenceChanger( mCalendar, this ) );
 
   mCalendar->registerObserver( this );
