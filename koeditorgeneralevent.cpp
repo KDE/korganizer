d0fb3c5b731a79a389433363ad244f19b37039ff
Thomas McGuire <mcguire@kde.org>
2009-05-27 14:15:23 +0000

    Merged revisions 972860 via svnmerge from svn+ssh://tmcguire@svn.kde.org/home/kde/branches/KDE/4.2/kdepim
    ........
      r972860 | winterz | 2009-05-25 22:42:30 +0200 (Mon, 25 May 2009) | 6 lines
      
      backport SVN commit 972592 by winterz:
      
      Do not apply start or end times when applying an Event template unless we have no other choice.
      
      Fixes 190545, reverts the original fix for 169146, and should also fix novell bug443130
    ........
    
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=973540

--- koeditorgeneralevent.cpp
+++ koeditorgeneralevent.cpp
@@ -446,8 +446,10 @@ void KOEditorGeneralEvent::readEvent( Event *event, Calendar *calendar, bool isT
     // the rest is for the events only
     setDateTimes( event->dtStart(), event->dtEnd() );
   } else {
-    // set the start/end time from the template
-    setTimes( event->dtStart(), event->dtEnd() );
+    // set the start/end time from the template, only as a last resort #190545
+    if ( !event->dtStart().isValid() || !event->dtEnd().isValid() ) {
+      setTimes( event->dtStart(), event->dtEnd() );
+    }
   }
 
   switch( event->transparency() ) {
