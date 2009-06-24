86532914e34c3ee3b5987661e1a83b083100e3cd
Thomas McGuire <mcguire@kde.org>
2009-06-24 13:23:20 +0000

    Merged revisions 965206 via svnmerge from svn+ssh://tmcguire@svn.kde.org/home/kde/branches/kdepim/enterprise4/kdepim
    ................
      r965206 | winterz | 2009-05-08 14:44:48 +0200 (Fri, 08 May 2009) | 10 lines
      
      Merged revisions 964997 via svnmerge from 
      https://svn.kde.org/home/kde/branches/kdepim/enterprise/kdepim
      
      ........
        r964997 | mlaurent | 2009-05-07 15:35:01 -0400 (Thu, 07 May 2009) | 3 lines
        
        Fix kolab issue 2276
        "Calendar -> File -> Export -> Export Web Page" silently overwrites files
      ........
    ................
    
    svn path=/trunk/KDE/kdepim/; revision=986290

--- actionmanager.cpp
+++ actionmanager.cpp
@@ -1134,6 +1134,11 @@ void ActionManager::exportHTML( HTMLExportSettings *settings )
     return;
   }
 
+  if ( QFileInfo( settings->outputFile() ).exists() ) {
+    if(KMessageBox::questionYesNo( dialogParent(), i18n("Do you want to overwrite file \"%1\"", settings->outputFile()) ) == KMessageBox::No)
+      return;
+  }
+
   QApplication::setOverrideCursor( QCursor ( Qt::WaitCursor ) );
   settings->setEMail( KOPrefs::instance()->email() );
   settings->setName( KOPrefs::instance()->fullName() );
