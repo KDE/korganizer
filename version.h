6bac0718d35cd46f9cb239d2bc04ab432fd90fbe
Thomas McGuire <mcguire@kde.org>
2009-08-07 09:36:42 +0000

    SVN_SILENT Update version numbers for today's release.
    svn path=/branches/kdepim/enterprise/kdepim/; revision=1008354

--- version.h
+++ version.h
@@ -53,6 +53,6 @@
     3.2 alpha1
 */
 
-static const char korgVersion[] = "3.5.9 (enterprise35 0.20090731.1005176)";
+static const char korgVersion[] = "3.5.9 (enterprise35 0.20090807.1008353)";
 
 #endif
