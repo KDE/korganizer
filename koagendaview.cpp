cb5562c8b0dcecc885d8caa4942569bfb8edc5bd
Allen Winter <winter@kde.org>
2009-09-23 16:49:31 +0000

    Merged revisions 1027235 via svnmerge from https://svn.kde.org/home/kde/branches/kdepim/enterprise/kdepim
    ........
      r1027235 | winterz | 2009-09-23 12:22:11 -0400 (Wed, 23 Sep 2009) | 3 lines
      
      in setHolidaysMasks(), avoid a crash if the selecteddates QList is empty.
      MERGE: e4,trunk,4.3
    ........
    
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=1027247

--- koagendaview.cpp
+++ koagendaview.cpp
@@ -1589,6 +1589,10 @@ void KOAgendaView::writeSettings( KConfig *config )
 
 void KOAgendaView::setHolidayMasks()
 {
+  if ( mSelectedDates.isEmpty() ) {
+    return;
+  }
+
   mHolidayMask.resize( mSelectedDates.count() + 1 );
 
   for ( int i = 0; i < mSelectedDates.count(); ++i ) {
