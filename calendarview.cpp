51ebaff063b56fdad15aac2bbd19c36cbb019dda
Allen Winter <winter@kde.org>
2009-09-28 16:26:15 +0000

    do'h
    svn path=/trunk/KDE/kdepim/; revision=1028962

--- calendarview.cpp
+++ calendarview.cpp
@@ -207,8 +207,8 @@ CalendarView::CalendarView( QWidget *parent )
   connect( this, SIGNAL(configChanged()),
            mDateNavigatorContainer, SLOT(updateConfig()) );
 
-  connect( this, SIGNAL(incidenceSelected(Incidence *,const QDate &date)),
-           mEventViewer, SLOT(setIncidence(Incidence *,const QDate &date)) );
+  connect( this, SIGNAL(incidenceSelected(Incidence *,const QDate &)),
+           mEventViewer, SLOT(setIncidence(Incidence *,const QDate &)) );
 
   //TODO: do a pretty Summary,
   QString s;
@@ -1781,7 +1781,7 @@ void CalendarView::processIncidenceSelection( Incidence *incidence, const QDate 
   }
 
   mSelectedIncidence = incidence;
-  mEventViewer->setIncidence( mSelectedIncidence, date );
+  emit incidenceSelected( mSelectedIncidence, date );
 
   bool organizerEvents = false;
   bool groupEvents = false;
