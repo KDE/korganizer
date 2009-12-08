435e27f890c4134dbfbf55beb2dbab501914221f
Allen Winter <winter@kde.org>
2009-12-08 21:04:46 +0000

    merge SVN commit 1060407 by winterz from trunk:
    Don't assert when adding a new Journal with attendees.
    Looks like I'm adding a new string, but I'm not: I'm reusing a string from 4.3
    
    MERGE: e4 (already in trunk)
    
    svn path=/branches/kdepim/enterprise/kdepim/; revision=1060410

--- kogroupware.cpp
+++ kogroupware.cpp
@@ -279,7 +279,6 @@ bool KOGroupware::sendICalMessage( QWidget* parent,
               arg( incidence->summary() );
         break;
       case KOGlobals::INCIDENCEDELETED:
-        Q_ASSERT( incidence->type() == "Event" || incidence->type() == "Todo" );
         if ( incidence->type() == "Event" ) {
           txt = i18n( "You removed the invitation \"%1\".\n"
                       "Do you want to email the attendees that the event is canceled?" ).
@@ -289,6 +288,10 @@ bool KOGroupware::sendICalMessage( QWidget* parent,
           txt = i18n( "You removed the invitation \"%1\".\n"
                       "Do you want to email the attendees that the todo is canceled?" ).
                 arg( incidence->summary() );
+        } else {
+          QString type = incidence->type();
+          txt = i18n( "This %1 includes other people. "
+                      "Should email be sent out to the attendees?" ).arg( type );
         }
         break;
       case KOGlobals::INCIDENCEADDED:
