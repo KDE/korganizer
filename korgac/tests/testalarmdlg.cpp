0b80106173544fff8e5786b32be75344d14c3f57
Volker Krause <vkrause@kde.org>
2010-01-07 10:18:09 +0000

    SVN_MERGE
    Merged revisions 1036660 via svnmerge from 
    https://vkrause@svn.kde.org/home/kde/trunk/KDE/kdepim
    
    ........
      r1036660 | winterz | 2009-10-17 17:11:04 +0200 (Sat, 17 Oct 2009) | 5 lines
      
      add the ability to sort the columns
      kolab/issue3809
      MERGE: e35,e4
      DO_NOT_BACKPORT: new strings and a new feature
    ........
    
    svn path=/branches/work/akonadi-ports/kdepim/; revision=1071013

--- korgac/tests/testalarmdlg.cpp
+++ korgac/tests/testalarmdlg.cpp
@@ -58,7 +58,7 @@ int main( int argc, char **argv )
   e1->setDtEnd( now.addDays( 1 ) );
   Alarm *a = e1->newAlarm();
 //  a->setProcedureAlarm( "/usr/X11R6/bin/xeyes" );
-  a->setAudioAlarm( "/opt/kde/share/apps/korganizer/sounds/spinout.wav" );
+  a->setAudioAlarm( "/data/kde/share/apps/korganizer/sounds/spinout.wav" );
 
   Todo *t1 = new Todo;
   t1->setSummary( "To-do A" );
@@ -68,14 +68,14 @@ int main( int argc, char **argv )
   Event *e2 = new Event;
   e2->setSummary( "This is another summary. "
                   "But it is a very long summary of total sillyness for no good reason" );
-  e2->setDtStart( now );
-  e2->setDtEnd( now.addDays( 1 ) );
+  e2->setDtStart( now.addDays( 1 ) );
+  e2->setDtEnd( now.addDays( 2 ) );
   e2->newAlarm();
 
   Event *e3 = new Event;
   e3->setSummary( "Meet with Fred" );
-  e3->setDtStart( now );
-  e3->setDtEnd( now.addDays( 2 ) );
+  e3->setDtStart( now.addDays( 2 ) );
+  e3->setDtEnd( now.addDays( 3 ) );
   e3->newAlarm();
 
   Todo *t2 = new Todo;
@@ -88,13 +88,27 @@ int main( int argc, char **argv )
   t3->setDtDue( now );
   t3->newAlarm();
 
+  Event *e4 = new Event;
+  e4->setSummary( "Watch TV" );
+  e4->setDtStart( now.addSecs( 120 ) );
+  e4->setDtEnd( now.addSecs( 180 ) );
+  e4->newAlarm();
+
   AlarmDialog dlg( 0 );
-  dlg.addIncidence( incidenceToItem(e1), QDateTime::currentDateTime(), QString() );
-  dlg.addIncidence( incidenceToItem(t1), QDateTime::currentDateTime(), QString( "THIS IS DISPLAY TEXT" ) );
-  dlg.addIncidence( incidenceToItem(e2), QDateTime::currentDateTime(), QString() );
-  dlg.addIncidence( incidenceToItem(e3), QDateTime::currentDateTime(), QString() );
-  dlg.addIncidence( incidenceToItem(t2), QDateTime::currentDateTime(), QString( "THIS IS DISPLAY TEXT" ) );
-  dlg.addIncidence( incidenceToItem(t3), QDateTime::currentDateTime(), QString() );
+  dlg.addIncidence( incidenceToItem(e2), QDateTime::currentDateTime().addSecs( 60 ),
+                    QString() );
+  dlg.addIncidence( incidenceToItem(t1), QDateTime::currentDateTime().addSecs( 300 ),
+                    QString( "THIS IS DISPLAY TEXT" ) );
+  dlg.addIncidence( incidenceToItem(e4), QDateTime::currentDateTime().addSecs( 120 ),
+                    QString( "Fred and Barney get cloned" ) );
+  dlg.addIncidence( incidenceToItem(e3), QDateTime::currentDateTime().addSecs( 240 ),
+                    QString() );
+  dlg.addIncidence( incidenceToItem(e1), QDateTime::currentDateTime().addSecs( 180 ),
+                    QString() );
+  dlg.addIncidence( incidenceToItem(t2), QDateTime::currentDateTime().addSecs( 600 ),
+                    QString( "THIS IS DISPLAY TEXT" ) );
+  dlg.addIncidence( incidenceToItem(t3), QDateTime::currentDateTime().addSecs( 360 ),
+                    QString() );
   dlg.show();
   dlg.eventNotification();
 
