855870b5d59f7b9b74ec4d2fa270095a87d35976
Allen Winter <winter@kde.org>
2010-04-09 11:43:12 +0000

    revert r1107542 as it causes more problems than it fixes. (this was intended to fix layout problems in the all-day area) MERGE: trunk,4.4
    svn path=/branches/kdepim/enterprise/kdepim/; revision=1112869

--- koagenda.cpp
+++ koagenda.cpp
@@ -1817,17 +1817,12 @@ void KOAgenda::resizeEvent ( QResizeEvent *ev )
 
 void KOAgenda::resizeAllContents()
 {
+  double subCellWidth;
   if ( mItems.count() > 0 ) {
-    double subCellWidth;
     KOAgendaItem *item;
     if (mAllDayMode) {
-      subCellWidth = 10000.00; // some large value
-      // first compute the smallest cell width for all the items
       for ( item=mItems.first(); item != 0; item=mItems.next() ) {
-        subCellWidth = QMIN( subCellWidth, calcSubCellWidth( item ) );
-      }
-      // now use that smallest cell width when placing each item
-      for ( item=mItems.first(); item != 0; item=mItems.next() ) {
+        subCellWidth = calcSubCellWidth( item );
         placeAgendaItem( item, subCellWidth );
       }
     } else {
