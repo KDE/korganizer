47db624a48659cfc603b05a23321f165e12d3cd8
Mike McQuaid <mike@mikemcquaid.com>
2010-06-30 11:57:35 +0000

    Merged r1144618 from trunk to 4.5 branch.
    svn path=/branches/KDE/4.5/kdepim/; revision=1144623

--- views/journalview/journalview.cpp
+++ views/journalview/journalview.cpp
@@ -3,7 +3,7 @@
 
   Copyright (c) 2001 Cornelius Schumacher <schumacher@kde.org>
   Copyright (C) 2003-2004 Reinhold Kainhofer <reinhold@kainhofer.com>
-  Copyright (c) 2007 Mike Arthur <mike@mikearthur.co.uk>
+  Copyright (c) 2007 Mike McQuaid <mike@mikemcquaid.com>
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
