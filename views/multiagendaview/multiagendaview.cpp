d667c38dc2f45c9bda4c4e1f64e6ee765ba61099
Sergio Luis Martins <iamsergio@gmail.com>
2010-09-01 21:41:02 +0000

    Same optimization as made in agenda view.
    svn path=/branches/work/~smartins/korganizer/; revision=1170741

--- views/multiagendaview/multiagendaview.cpp
+++ views/multiagendaview/multiagendaview.cpp
@@ -109,7 +109,20 @@ void MultiAgendaView::updateConfig()
 
 void MultiAgendaView::setChanges( EventViews::EventView::Changes changes )
 {
-  d->mMultiAgendaView->setChanges( changes );
+  // Only ConfigChanged and FilterChanged should go from korg->AgendaView
+  // All other values are already detected inside AgendaView.
+  // We could just pass "changes", but korganizer does a very bad job at
+  // determining what changed, for example if you move an incidence
+  // the BaseView::setDateRange(...) is called causing DatesChanged
+  // flag to be on, when no dates changed.
+  EventViews::EventView::Changes c;
+  if ( changes.testFlag( EventViews::EventView::ConfigChanged ) )
+    c = EventViews::EventView::ConfigChanged;
+
+  if ( changes.testFlag( EventViews::EventView::FilterChanged ) )
+    c |= EventViews::EventView::FilterChanged;
+
+  d->mMultiAgendaView->setChanges( c | d->mMultiAgendaView->changes() );
 }
 
 bool MultiAgendaView::hasConfigurationDialog() const
