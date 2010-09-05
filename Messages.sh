1e2c8747e56d70928cb54f1c70006372087071f6
Sergio Luis Martins <iamsergio@gmail.com>
2010-09-05 19:10:21 +0000

    Copying ~smartins/korganizer -> trunk.
    Korganizer now uses kdepim/calendarviews.
    
    svn path=/trunk/KDE/kdepim/korganizer/; revision=1171936

--- Messages.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-$PREPARETIPS > tips.cpp
-$EXTRACTRC `find . -name "*.rc" -o -name "*.ui" -o -name "*.kcfg"` >> rc.cpp || exit 11
-$XGETTEXT `find . -name "*.cpp" -o -name "*.h"` -o $podir/korganizer.pot
-rm -f tips.cpp
-rm -f rc.cpp
