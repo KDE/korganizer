0b701b9e2fa54669631385e51c29fed7ec1056eb
Allen Winter <winter@kde.org>
2010-09-21 16:57:22 +0000

    SVN_SILENT Update version numbers for today's release.
    svn path=/branches/kdepim/enterprise/kdepim/; revision=1177913

--- version.h
+++ version.h
@@ -53,6 +53,6 @@
     3.2 alpha1
 */
 
-static const char korgVersion[] = "3.5.9 (enterprise35 0.20100921.1177909)";
+static const char korgVersion[] = "3.5.9 (enterprise35 0.20100921.1177912)";
 
 #endif
