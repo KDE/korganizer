aee04a0f07bde293343daa163b30272b17be9870
Pino Toscano <pino@kde.org>
2010-11-01 13:41:39 +0000

    do not extract messages from sources in 'tests' subdirectories
    svn path=/trunk/KDE/kdepim/korganizer/; revision=1191936

--- Messages.sh
+++ Messages.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 $PREPARETIPS > tips.cpp
 $EXTRACTRC `find . -name "*.rc" -o -name "*.ui" -o -name "*.kcfg"` >> rc.cpp || exit 11
-$XGETTEXT `find . -name "*.cpp" -o -name "*.h"` -o $podir/korganizer.pot
+$XGETTEXT `find . -name "*.cpp" -o -name "*.h" | grep -v '/tests/'` -o $podir/korganizer.pot
 rm -f tips.cpp
 rm -f rc.cpp
