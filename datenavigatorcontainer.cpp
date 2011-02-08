246ce8f763af5e7caf5afd450c889211cb375648
Sergio Martins <iamsergio@gmail.com>
2011-02-08 00:19:37 +0000

    Remove const& from signals.

--- datenavigatorcontainer.cpp
+++ datenavigatorcontainer.cpp
@@ -68,23 +68,23 @@ DateNavigatorContainer::~DateNavigatorContainer()
 
 void DateNavigatorContainer::connectNavigatorView( KDateNavigator *v )
 {
-  connect( v, SIGNAL(datesSelected(const KCalCore::DateList &)),
-           SIGNAL(datesSelected(const KCalCore::DateList &)) );
+  connect( v, SIGNAL(datesSelected(KCalCore::DateList)),
+           SIGNAL(datesSelected(KCalCore::DateList)) );
 
   connect( v, SIGNAL(incidenceDropped(Akonadi::Item,QDate)),
            SIGNAL(incidenceDropped(Akonadi::Item,QDate)) );
   connect( v, SIGNAL(incidenceDroppedMove(Akonadi::Item,QDate)),
            SIGNAL(incidenceDroppedMove(Akonadi::Item,QDate)) );
 
-  connect( v, SIGNAL(newEventSignal(const QDate &)),
-           SIGNAL(newEventSignal(const QDate &)) );
-  connect( v, SIGNAL(newTodoSignal(const QDate &)),
-           SIGNAL(newTodoSignal(const QDate &)) );
-  connect( v, SIGNAL(newJournalSignal(const QDate &)),
-           SIGNAL(newJournalSignal(const QDate &)) );
+  connect( v, SIGNAL(newEventSignal(QDate)),
+           SIGNAL(newEventSignal(QDate)) );
+  connect( v, SIGNAL(newTodoSignal(QDate)),
+           SIGNAL(newTodoSignal(QDate)) );
+  connect( v, SIGNAL(newJournalSignal(QDate)),
+           SIGNAL(newJournalSignal(QDate)) );
 
-  connect( v, SIGNAL(weekClicked(const QDate &)),
-           SIGNAL(weekClicked(const QDate &)) );
+  connect( v, SIGNAL(weekClicked(QDate)),
+           SIGNAL(weekClicked(QDate)) );
 
   connect( v, SIGNAL(goPrevious()), SIGNAL(goPrevious()) );
   connect( v, SIGNAL(goNext()), SIGNAL(goNext()) );
