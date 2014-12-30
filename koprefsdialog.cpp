575582dde18431019d21dbbef60b8a98a565e6c5
Montel Laurent <montel@kde.org>
2014-12-30 09:39:41 +0100

    Minor optimization

--- koprefsdialog.cpp
+++ koprefsdialog.cpp
@@ -1281,7 +1281,8 @@ void KOPrefsDialogPlugins::usrReadConfig()
                            i18nc( "@title:group", "Other Plugins" ) ) );
 
   KService::List::ConstIterator it;
-  for ( it = plugins.constBegin(); it != plugins.constEnd(); ++it ) {
+  KService::List::ConstIterator end(plugins.constEnd());
+  for ( it = plugins.constBegin(); it != end; ++it ) {
     QTreeWidgetItem *item = 0;
     if ( (*it)->hasServiceType( EventViews::CalendarDecoration::Decoration::serviceType() ) ) {
       item = new PluginItem( decorations, *it );
@@ -1312,7 +1313,7 @@ void KOPrefsDialogPlugins::usrWriteConfig()
 
   for ( int i = 0; i < mTreeWidget->topLevelItemCount(); ++i) {
     QTreeWidgetItem *serviceTypeGroup = mTreeWidget->topLevelItem( i );
-    for ( int j = 0; j < serviceTypeGroup->childCount(); j++) {
+    for ( int j = 0; j < serviceTypeGroup->childCount(); ++j) {
       PluginItem *item = static_cast<PluginItem *>( serviceTypeGroup->child( j ) );
       if( item->checkState( 0 ) == Qt::Checked ) {
         selectedPlugins.append( item->service()->desktopEntryName() );
