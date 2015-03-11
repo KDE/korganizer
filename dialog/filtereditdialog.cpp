d7595479ab0f11715b4828a1512c927ac249d949
Montel Laurent <montel@kde.org>
2015-03-11 08:27:27 +0100

    Fix create unique filter name when we click on "new"

--- dialog/filtereditdialog.cpp
+++ dialog/filtereditdialog.cpp
@@ -225,9 +225,20 @@ void FilterEdit::bNewPressed()
 {
   mDetailsFrame->setEnabled( true );
   saveChanges();
+  QStringList filterNames;
+  for (int i=0; i < mRulesList->count(); ++i) {
+      filterNames << mRulesList->item(i)->text();
+  }
+  QString newFilterName;
+  for (int i = 1;;++i) {
+      newFilterName = i18nc( "@label default filter name",
+                             "New Filter %1", i );
+      if (!filterNames.contains(newFilterName)) {
+          break;
+      }
+  }
   KCalCore::CalFilter *newFilter =
-    new KCalCore::CalFilter( i18nc( "@label default filter name",
-                                    "New Filter %1", mFilters->count() ) );
+    new KCalCore::CalFilter( newFilterName );
   mFilters->append( newFilter );
   updateFilterList();
   mRulesList->setCurrentRow( mRulesList->count() - 1 );
