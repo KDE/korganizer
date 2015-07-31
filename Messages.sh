8f3e83c397a89a4f4836967a4ad8686ef31cd0d5
Montel Laurent <montel@kde.org>
2015-07-31 06:42:59 +0200

    tips was removed

--- Messages.sh
+++ Messages.sh
@@ -1,6 +1,4 @@
 #! /bin/sh
-$PREPARETIPS > tips.cpp
 $EXTRACTRC `find . -name "*.rc" -o -name "*.ui" -o -name "*.kcfg"` >> rc.cpp || exit 11
 $XGETTEXT `find . -name "*.cpp" -o -name "*.h" | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/korganizer.pot
-rm -f tips.cpp
 rm -f rc.cpp
