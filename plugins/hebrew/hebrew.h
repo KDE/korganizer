62f5cb20a57aecaa9009ab7c37e3c9288fb323b2
Volker Krause <vkrause@kde.org>
2015-08-16 10:33:04 +0200

    Silence -pedantic warning.

--- plugins/hebrew/hebrew.h
+++ plugins/hebrew/hebrew.h
@@ -46,7 +46,7 @@ private:
 class HebrewFactory : public DecorationFactory
 {
     Q_OBJECT
-    Q_PLUGIN_METADATA(IID "org.kde.korganizer.Hebrew");
+    Q_PLUGIN_METADATA(IID "org.kde.korganizer.Hebrew")
 public:
     Decoration *createPluginFactory() Q_DECL_OVERRIDE {
         return new Hebrew;
