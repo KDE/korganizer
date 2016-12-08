e18dd3f346975a5f05b9ff9b25ea25be72e931c3
Montel Laurent <montel@kde.org>
2016-12-08 08:19:49 +0100

    Improve

--- src/manageshowcollectionproperties.cpp
+++ src/manageshowcollectionproperties.cpp
@@ -22,7 +22,9 @@
 ManageShowCollectionProperties::ManageShowCollectionProperties(QObject *parent)
     : QObject(parent)
 {
-
+    mPages = QStringList() << QStringLiteral("CalendarSupport::CollectionGeneralPage")
+                           << QStringLiteral("Akonadi::CachePolicyPage")
+                           << QStringLiteral("PimCommon::CollectionAclPage");
 }
 
 ManageShowCollectionProperties::~ManageShowCollectionProperties()
@@ -32,14 +34,64 @@ ManageShowCollectionProperties::~ManageShowCollectionProperties()
 
 void ManageShowCollectionProperties::showCollectionProperties()
 {
-
+#if 0
+    const Akonadi::Collection col = mMainWidget->currentAddressBook();
+    const Akonadi::Collection::Id id = col.id();
+    QPointer<Akonadi::CollectionPropertiesDialog> dlg = mHashDialogBox.value(id);
+    if (dlg) {
+        dlg->activateWindow();
+        dlg->raise();
+        return;
+    }
+    Akonadi::CollectionAttributesSynchronizationJob *sync
+        = new Akonadi::CollectionAttributesSynchronizationJob(col);
+    sync->setProperty("collectionId", id);
+    connect(sync, &KJob::result,
+            this, &ManageShowCollectionProperties::slotCollectionPropertiesContinued);
+    sync->start();
+#endif
 }
 
 void ManageShowCollectionProperties::slotCollectionPropertiesContinued(KJob *job)
 {
-
+#if 0
+    QString pageToShow;
+    if (job) {
+        Akonadi::CollectionAttributesSynchronizationJob *sync
+            = qobject_cast<Akonadi::CollectionAttributesSynchronizationJob *>(job);
+        Q_ASSERT(sync);
+        if (sync->property("collectionId") != mMainWidget->currentAddressBook().id()) {
+            return;
+        }
+    }
+    Akonadi::CollectionFetchJob *fetch = new Akonadi::CollectionFetchJob(mMainWidget->currentAddressBook(),
+            Akonadi::CollectionFetchJob::Base);
+    connect(fetch, &KJob::result,
+            this, &ManageShowCollectionProperties::slotCollectionPropertiesFinished);
+#endif
 }
 
 void ManageShowCollectionProperties::slotCollectionPropertiesFinished(KJob *job)
 {
+#if 0
+    if (!job) {
+        return;
+    }
+
+    Akonadi::CollectionFetchJob *fetch = qobject_cast<Akonadi::CollectionFetchJob *>(job);
+    Q_ASSERT(fetch);
+    if (fetch->collections().isEmpty()) {
+        qCWarning(KADDRESSBOOK_LOG) << "no collection";
+        return;
+    }
+
+    const Akonadi::Collection collection = fetch->collections().first();
+
+    QPointer<Akonadi::CollectionPropertiesDialog> dlg = new Akonadi::CollectionPropertiesDialog(collection, mPages, mMainWidget);
+    dlg->setWindowTitle(i18nc("@title:window", "Properties of Address Book Folder %1", collection.name()));
+
+    dlg->show();
+    mHashDialogBox.insert(collection.id(), dlg);
+#endif
 }
+
