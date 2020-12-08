865d0b217f3db53ef7b0b9e0c86951b573df5d24
Laurent Montel <montel@kde.org>
2020-12-08 14:00:21 +0100

    operator+/- is disabled for QFlags in qt6

--- src/kontactplugin/korganizer/todoplugin.cpp
+++ src/kontactplugin/korganizer/todoplugin.cpp
@@ -39,7 +39,7 @@ TodoPlugin::TodoPlugin(KontactInterface::Core *core, const QVariantList &)
         = new QAction(QIcon::fromTheme(QStringLiteral("task-new")),
                       i18nc("@action:inmenu", "New To-do..."), this);
     actionCollection()->addAction(QStringLiteral("new_todo"), action);
-    actionCollection()->setDefaultShortcut(action, QKeySequence(Qt::CTRL + Qt::SHIFT + Qt::Key_T));
+    actionCollection()->setDefaultShortcut(action, QKeySequence(Qt::CTRL | Qt::SHIFT | Qt::Key_T));
     QString str = i18nc("@info:status", "Create a new to-do");
     action->setStatusTip(str);
     action->setToolTip(str);
