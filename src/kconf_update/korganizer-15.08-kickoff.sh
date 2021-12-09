0a2937e763e8c55df71b1cc9988a7296b01357e3
Laurent Montel <montel@kde.org>
2021-12-09 07:06:21 +0100

    GIT_SILENT: after 6 years we can remove it (already done in other pim*
    apps)

--- src/kconf_update/korganizer-15.08-kickoff.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-kickoffrcname=`qtpaths --locate-file  GenericConfigLocation kickoffrc`
-if [ -f "$kickoffrcname" ]; then
-   sed -i "s/\/korganizer.desktop/\/org.kde.korganizer.desktop/" $kickoffrcname
-fi
