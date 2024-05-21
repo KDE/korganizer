85bfda0f19fc180cb53aca08a297bb4b1c8aab06
Laurent Montel <montel@kde.org>
2024-05-21 08:09:16 +0200

    Use KIconThemes and KStyleManager if available during build-time
    This can benefit platforms like Windows so breeze's icon can be used
    if the platform don't offer icon theme support out-of-the-box.
    
    See also: https://cullmann.io/posts/kde-applications-and-icons/

--- src/main.cpp
+++ src/main.cpp
@@ -20,10 +20,31 @@
 #ifdef WITH_KUSERFEEDBACK
 #include "userfeedback/korganizeruserfeedbackprovider.h"
 #endif
+
+#define HAVE_KICONTHEME __has_include(<KIconTheme>)
+#if HAVE_KICONTHEME
+#include <KIconTheme>
+#endif
+
+#define HAVE_STYLE_MANAGER __has_include(<KStyleManager>)
+#if HAVE_STYLE_MANAGER
+#include <KStyleManager>
+#endif
+
 int main(int argc, char **argv)
 {
+#if HAVE_KICONTHEME && (KICONTHEMES_VERSION >= QT_VERSION_CHECK(6, 3, 0))
+    KIconTheme::initTheme();
+#endif
     KOrganizerApp app(argc, &argv);
     KCrash::initialize();
+#if HAVE_STYLE_MANAGER
+    KStyleManager::initStyle();
+#else // !HAVE_STYLE_MANAGER
+#if defined(Q_OS_MACOS) || defined(Q_OS_WIN)
+    QApplication::setStyle(QStringLiteral("breeze"));
+#endif // defined(Q_OS_MACOS) || defined(Q_OS_WIN)
+#endif // HAVE_STYLE_MANAGER
     KLocalizedString::setApplicationDomain(QByteArrayLiteral("korganizer"));
 
     KOrg::AboutData aboutData;
