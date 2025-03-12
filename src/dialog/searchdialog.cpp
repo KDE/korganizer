KDE/korganizer

commit f70c524086ea7697e3ba3fb0cecfb3a738858854

Add a search option to consider the current view filter
By default, the current view filter is ignored when searching.
The user can override that.

BUG: 499336

src/dialog/searchdialog.cpp
@@ -200,10 +200,20 @@ void SearchDialog::search(const QRegularExpression &regularExpression)
     for (const auto &calendar : m_calendarview->enabledCalendars()) {
         if (m_ui->dateRangeCheckbox->isChecked()) {
             if (m_ui->eventsCheck->isChecked()) {
-                events += calendar->events(startDt, endDt, QTimeZone::systemTimeZone(), m_ui->inclusiveCheck->isChecked());
+                if (m_ui->unfiltered->isChecked()) {
+                    events += calendar->rawEvents(startDt, endDt, QTimeZone::systemTimeZone(), m_ui->inclusiveCheck->isChecked());
+                } else {
+                    events += calendar->events(startDt, endDt, QTimeZone::systemTimeZone(), m_ui->inclusiveCheck->isChecked());
+                }
             }
             if (m_ui->includeUndatedTodos->isChecked()) {
-                for (const KCalendarCore::Todo::Ptr &todo : calendar->todos()) {
+                KCalendarCore::Todo::List tempTodos;
+                if (m_ui->unfiltered->isChecked()) {
+                    KCalendarCore::Todo::List tmpTodos = calendar->rawTodos();
+                } else {
+                    KCalendarCore::Todo::List tmpTodos = calendar->todos();
+                }
+                for (const KCalendarCore::Todo::Ptr &todo : tempTodos) {
                     Q_ASSERT(todo);
                     if ((!todo->hasStartDate() && !todo->hasDueDate()) // undated
                         || (todo->hasStartDate() && (todo->dtStart().toLocalTime().date() >= startDt)
@@ -216,24 +226,44 @@ void SearchDialog::search(const QRegularExpression &regularExpression)
                     }
                 }
             } else {
-                todos += calendar->todos(startDt, endDt, QTimeZone::systemTimeZone(), m_ui->inclusiveCheck->isChecked());
+                if (m_ui->unfiltered->isChecked()) {
+                    todos += calendar->rawTodos(startDt, endDt, QTimeZone::systemTimeZone(), m_ui->inclusiveCheck->isChecked());
+                } else {
+                    todos += calendar->todos(startDt, endDt, QTimeZone::systemTimeZone(), m_ui->inclusiveCheck->isChecked());
+                }
             }
 
             if (m_ui->journalsCheck->isChecked()) {
                 for (auto dt = startDt; dt <= endDt; dt = dt.addDays(1)) {
-                    journals += calendar->journals(dt);
+                    if (m_ui->unfiltered->isChecked()) {
+                        journals += calendar->rawJournalsForDate(dt);
+                    } else {
+                        journals += calendar->journals(dt);
+                    }
                 }
             }
 
         } else {
             if (m_ui->eventsCheck->isChecked()) {
-                events += calendar->events();
+                if (m_ui->unfiltered->isChecked()) {
+                    events += calendar->rawEvents();
+                } else {
+                    events += calendar->events();
+                }
             }
             if (m_ui->todosCheck->isChecked()) {
-                todos += calendar->todos();
+                if (m_ui->unfiltered->isChecked()) {
+                    todos += calendar->rawTodos();
+                } else {
+                    todos += calendar->todos();
+                }
             }
             if (m_ui->journalsCheck->isChecked()) {
-                journals += calendar->journals();
+                if (m_ui->unfiltered->isChecked()) {
+                    journals += calendar->rawJournals();
+                } else {
+                    journals += calendar->journals();
+                }
             }
         }
     }

src/ui/searchdialog_base.ui
@@ -197,8 +197,8 @@
      <property name="title">
       <string>Date Range</string>
      </property>
-     <layout class="QFormLayout" name="formLayout">
-      <item row="0" column="0" colspan="2">
+     <layout class="QVBoxLayout" name="verticalLayout_2">
+      <item>
        <widget class="QCheckBox" name="dateRangeCheckbox">
         <property name="toolTip">
          <string>When checked, the search will be limited to the specified date range.</string>
@@ -214,45 +214,7 @@
         </property>
        </widget>
       </item>
-      <item row="1" column="0">
-       <layout class="QHBoxLayout" name="horizontalLayout_2">
-        <item>
-         <widget class="QLabel" name="startDateLabel">
-          <property name="text">
-           <string>Fr&amp;om:</string>
-          </property>
-          <property name="wordWrap">
-           <bool>false</bool>
-          </property>
-          <property name="buddy">
-           <cstring>startDate</cstring>
-          </property>
-         </widget>
-        </item>
-        <item>
-         <widget class="KDateComboBox" name="startDate">
-          <property name="sizePolicy">
-           <sizepolicy hsizetype="Preferred" vsizetype="Fixed">
-            <horstretch>1</horstretch>
-            <verstretch>0</verstretch>
-           </sizepolicy>
-          </property>
-          <property name="toolTip">
-           <string>The starting date of the period to search.</string>
-          </property>
-          <property name="whatsThis">
-           <string>Search for items in the time period starting at the date given here.</string>
-          </property>
-          <item>
-           <property name="text">
-            <string>2008-08-18</string>
-           </property>
-          </item>
-         </widget>
-        </item>
-       </layout>
-      </item>
-      <item row="1" column="1">
+      <item>
        <layout class="QHBoxLayout" name="horizontalLayout_3">
         <item>
          <widget class="QLabel" name="endDateLabel">
@@ -290,7 +252,45 @@
         </item>
        </layout>
       </item>
-      <item row="2" column="0" colspan="2">
+      <item>
+       <layout class="QHBoxLayout" name="horizontalLayout_2">
+        <item>
+         <widget class="QLabel" name="startDateLabel">
+          <property name="text">
+           <string>Fr&amp;om:</string>
+          </property>
+          <property name="wordWrap">
+           <bool>false</bool>
+          </property>
+          <property name="buddy">
+           <cstring>startDate</cstring>
+          </property>
+         </widget>
+        </item>
+        <item>
+         <widget class="KDateComboBox" name="startDate">
+          <property name="sizePolicy">
+           <sizepolicy hsizetype="Preferred" vsizetype="Fixed">
+            <horstretch>1</horstretch>
+            <verstretch>0</verstretch>
+           </sizepolicy>
+          </property>
+          <property name="toolTip">
+           <string>The starting date of the period to search.</string>
+          </property>
+          <property name="whatsThis">
+           <string>Search for items in the time period starting at the date given here.</string>
+          </property>
+          <item>
+           <property name="text">
+            <string>2008-08-18</string>
+           </property>
+          </item>
+         </widget>
+        </item>
+       </layout>
+      </item>
+      <item>
        <widget class="QCheckBox" name="inclusiveCheck">
         <property name="toolTip">
          <string>Display events only if their start date and end date are both in the date range specified.</string>
@@ -303,7 +303,7 @@
         </property>
        </widget>
       </item>
-      <item row="3" column="0" colspan="2">
+      <item>
        <widget class="QCheckBox" name="includeUndatedTodos">
         <property name="toolTip">
          <string>Include to-dos that have a start date, due date,  or completion date in the date range specified, or have neither a start date nor an end date.</string>
@@ -313,6 +313,22 @@
         </property>
         <property name="text">
          <string>Include all to-dos &amp;within the specified date range</string>
+        </property>
+        <property name="checked">
+         <bool>true</bool>
+        </property>
+       </widget>
+      </item>
+      <item>
+       <widget class="QCheckBox" name="unfiltered">
+        <property name="toolTip">
+         <string>Uncheck this box to skip incidences matching the currently selected view filter</string>
+        </property>
+        <property name="whatsThis">
+         <string>When this box is checked all incidences wil be considered in the search, even those that aren't displayed according the currently selected view filter</string>
+        </property>
+        <property name="text">
+         <string>Include all incidences matched by view filter</string>
         </property>
         <property name="checked">
          <bool>true</bool>
@@ -338,10 +354,10 @@
       </sizepolicy>
      </property>
      <property name="frameShape">
-      <enum>QFrame::StyledPanel</enum>
+      <enum>QFrame::Shape::StyledPanel</enum>
      </property>
      <property name="frameShadow">
-      <enum>QFrame::Raised</enum>
+      <enum>QFrame::Shadow::Raised</enum>
      </property>
     </widget>
    </item>

commit 790be5042eb1b2c8600fca9dd0f724963227c0f2

searchdialog.cpp - fix searching for undated to-dos in date-range
another bug I introduced with f70c524086ea7697e3ba3fb0cecfb3a738858854

src/dialog/searchdialog.cpp
@@ -207,13 +207,13 @@ void SearchDialog::search(const QRegularExpression &regularExpression)
                 }
             }
             if (m_ui->includeUndatedTodos->isChecked()) {
-                KCalendarCore::Todo::List tempTodos;
+                KCalendarCore::Todo::List tmpTodos;
                 if (m_ui->unfiltered->isChecked()) {
-                    KCalendarCore::Todo::List tmpTodos = calendar->rawTodos();
+                    tmpTodos = calendar->rawTodos();
                 } else {
-                    KCalendarCore::Todo::List tmpTodos = calendar->todos();
+                    tmpTodos = calendar->todos();
                 }
-                for (const KCalendarCore::Todo::Ptr &todo : tempTodos) {
+                for (const KCalendarCore::Todo::Ptr &todo : tmpTodos) {
                     Q_ASSERT(todo);
                     if ((!todo->hasStartDate() && !todo->hasDueDate()) // undated
                         || (todo->hasStartDate() && (todo->dtStart().toLocalTime().date() >= startDt)