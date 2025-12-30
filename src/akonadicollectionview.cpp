bf8677379f74c8ebb5296da93bc8827531f21366
Simran0777 <simran62957@gmail.com>
2025-12-30 18:36:09 +0530

    First PR: added a small comment

--- src/akonadicollectionview.cpp
+++ src/akonadicollectionview.cpp
@@ -9,7 +9,7 @@
 
   SPDX-License-Identifier: GPL-2.0-or-later WITH LicenseRef-Qt-Commercial-exception-1.0
 */
-
+// First contribution: adding a small comment
 #include "akonadicollectionview.h"
 #include "collectionsortfilterproxymodel.h"
 #include "config-korganizer.h"
